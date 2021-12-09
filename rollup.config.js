16bda4bc3ed5d4e77bedf3732324a71b1e9520e0
Wilmer H. Muñoz <wmunoz@talenta.cloud>
2021-12-09 15:36:42 -0500

    build: antd

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import postcss from 'rollup-plugin-postcss'
 import dts from 'rollup-plugin-dts'
-
-const packageJson = require('./package.json')
+import packageJson from './package.json'
 
 export default [
   {
@@ -24,7 +23,7 @@ export default [
     plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), postcss()]
   },
   {
-    input: 'dist/esm/types/index.d.ts',
+    input: 'dist/esm/types/components/Layout/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
     external: [/\.(css|less|scss)$/]
