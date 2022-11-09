cf7e4aa23164f4b3cc0c6ba669566014e972ac6a
Wilmer H Muñoz <wmunoz@talenta.cloud>
2022-11-09 05:49:24 -0500

    docs: change your region

--- src/components/Layout/NavigationDrawer.Stories.tsx
+++ src/components/Layout/NavigationDrawer.Stories.tsx
@@ -1,35 +1,30 @@
 import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 import NavigationDrawer from './NavigationDrawer.Component'
+import 'antd/dist/antd.compact.css'
 
 export default {
   title: 'NavigationDrawer',
-  component: NavigationDrawer
+  component: NavigationDrawer,
+  args: {
+    lang: 'es-CO',
+    visible: true,
+    placement: 'left',
+    width: 300,
+    onClose: action('onClose'),
+    onRegionChange: action('onRegionChange'),
+    queryString: {
+      lng: 'es-CO',
+      access_token: 'YWJjMTIz'
+    }
+  }
 } as ComponentMeta<typeof NavigationDrawer>
 
 const Template: ComponentStory<typeof NavigationDrawer> = (args) => <NavigationDrawer {...(args as never)} />
 
-export const HelloDrawer = Template.bind({})
-HelloDrawer.storyName = 'Mounting drawer'
-HelloDrawer.args = {
-  lang: 'es-CO',
-  visible: true,
-  placement: 'left',
-  width: 300,
-  onClose: () => console.info('Closing drawer'),
-  queryString: {
-    lng: 'es-CO',
-    access_token: 'YWJjMTIz'
-  }
-}
+export const MountDrawer = Template.bind({})
+MountDrawer.storyName = 'Mounting drawer'
 
 export const ChangeRegion = Template.bind({})
-
-ChangeRegion.storyName = 'On Change Region'
-ChangeRegion.args = {
-  lang: 'es-CO',
-  visible: true,
-  placement: 'left',
-  width: 300,
-  onClose: () => console.info('Closing drawer')
-}
+ChangeRegion.storyName = 'Select Region'
