c0b145280530b6cb646bb3c211fdd83ad86f06c3
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-04-26 11:55:13 -0500

    feat: icon atom has been created

---
+++ src/components/UI/atoms/Icon/Icon.component.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Link21 } from 'iconsax-react'
+import { IconProps } from './Icon.interface'
+
+const Icon: React.FC<IconProps> = ({ Icon = Link21, size = 20, color = '#2c2c2c', variant = 'Linear' }) => {
+  return <Icon variant={variant} size={size} color={color} />
+}
+
+export default Icon
---
+++ src/components/UI/atoms/Icon/Icon.interface.ts
@@ -0,0 +1,5 @@
+import { IconProps as IconPropsIconsSax, Icon } from 'iconsax-react'
+
+export interface IconProps extends IconPropsIconsSax {
+  Icon: Icon
+}
