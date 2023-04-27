2546b93a9bde9f4e7510f64dec84696def1a9f01
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-04-27 10:34:33 -0500

    fix: comments was removed

--- src/components/UI/molecules/ListIconLink/ListIconLink.interface.ts
+++ src/components/UI/molecules/ListIconLink/ListIconLink.interface.ts
@@ -4,21 +4,9 @@ type Direction = 'row' | 'column'
 type Variant = 'Linear' | 'Outline' | 'Broken' | 'Bold' | 'Bulk' | 'TwoTone'
 
 export interface IListIconLink {
-  /**
-   * Change direction
-   */
   direction: Direction
-  /**
-   * Change spacing between icons
-   */
   spacing: number
-  /**
-   * Add destination on click
-   */
   url: string
-  /**
-   * You can set Icon o get Default icon
-   */
   Icon: Icon
   listIcon: any
   variant: Variant
