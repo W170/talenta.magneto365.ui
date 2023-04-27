6a9fc5c3c1fed6775cbec3284dc429a9a37e93dd
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-04-27 10:21:31 -0500

    fix: the component was renamed

--- src/components/UI/atoms/Icon/Icon.component.tsx
+++ src/components/UI/atoms/Icon/Icon.component.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { Link21 } from 'iconsax-react'
 import { IconProps } from './Icon.interface'
 
-const Icon: React.FC<IconProps> = ({ Icon = Link21, size = 20, color = '#2c2c2c', variant = 'Linear' }) => {
+const IconItem: React.FC<IconProps> = ({ Icon = Link21, size = 20, color = '#2c2c2c', variant = 'Linear' }) => {
   return <Icon variant={variant} size={size} color={color} />
 }
 
-export default Icon
+export default IconItem
