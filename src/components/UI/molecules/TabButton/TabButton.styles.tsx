655b70debc0d353583df2ef3eafe68634bf35237
Juan S. Ballen <jballen@talenta.cloud>
2023-05-02 11:14:08 -0500

    styles: TabButton component modified

--- src/components/UI/molecules/TabButton/TabButton.styles.tsx
+++ src/components/UI/molecules/TabButton/TabButton.styles.tsx
@@ -2,19 +2,18 @@ import styled from 'styled-components'
 import { ITabButton } from './TabButton.interface'
 
 export const withStyles = (c: React.FC<ITabButton>): React.FC<ITabButton> => styled(c)`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
   width: 96px;
   height: 18px;
+  padding: 2px 8px;
   background: ${(props) => props.theme.colors.white};
   border-radius: 6px;
   box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.1);
 
   a {
-    display: flex;
-    flex-direction: row;
-    align-items: flex-start;
-    padding: 2px 8px;
     color: ${(props) => props.theme.colors.black};
-    font-style: normal;
     font-weight: 800;
     font-size: ${(props) => props.size};
     line-height: 14px;
