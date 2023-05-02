af28360cac5760c935a68d25a14ae4eab98206cb
Juan S. Ballen <jballen@talenta.cloud>
2023-05-02 09:38:46 -0500

    styles: updated variables with theme props

--- src/components/UI/molecules/TabButton/TabButton.styles.tsx
+++ src/components/UI/molecules/TabButton/TabButton.styles.tsx
@@ -4,7 +4,7 @@ import { ITabButton } from './TabButton.interface'
 export const withStyles = (c: React.FC<ITabButton>): React.FC<ITabButton> => styled(c)`
   width: 96px;
   height: 18px;
-  background: #ffffff;
+  background: ${(props) => props.theme.colors.white};
   border-radius: 6px;
   box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.1);
 
@@ -13,14 +13,14 @@ export const withStyles = (c: React.FC<ITabButton>): React.FC<ITabButton> => sty
     flex-direction: row;
     align-items: flex-start;
     padding: 2px 8px;
-    color: #14141c;
+    color: ${(props) => props.theme.colors.black};
     font-style: normal;
     font-weight: 800;
     font-size: ${(props) => props.size};
     line-height: 14px;
 
     :hover {
-      color: #14141c;
+      color: ${(props) => props.theme.colors.black};
     }
   }
 `
