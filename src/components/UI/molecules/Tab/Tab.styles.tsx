b108195852844acb3fab159e79b18f201c29fbd6
Juan S. Ballen <jballen@talenta.cloud>
2023-05-02 17:56:54 -0500

    styles: component refactor according guidelines

--- src/components/UI/molecules/Tab/Tab.styles.tsx
+++ src/components/UI/molecules/Tab/Tab.styles.tsx
@@ -1,33 +1,14 @@
 import styled from 'styled-components'
+import { ITab } from './Tab.interface'
 
-export const StyledTabContainer = styled.div`
+export const withStyles = (c: React.FC<ITab>): React.FC<ITab> => styled(c)`
   display: flex;
   flex-direction: row;
   align-items: center;
-  width: 188px;
+  gap: 4px;
+  width: 200px;
   height: 24px;
   padding: 2px;
   border-radius: 8px;
   background-color: ${(props) => props.theme.colors.$gray5};
-`
-
-export const StyledInsideTab = styled.div`
-  display: inherit;
-  justify-content: center;
-  align-items: center;
-  width: 82px;
-  height: 16px;
-  border-radius: 6px;
-
-  a {
-    font-style: normal;
-    font-weight: 400;
-    font-size: 11px;
-    line-height: 14px;
-    color: ${(props) => props.theme.colors.$grayDark};
-
-    :hover {
-      color: ${(props) => props.theme.colors.$grayDark};
-    }
-  }
 `
