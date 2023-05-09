670c3bc548cd7ec9acc4781251d56e53a0b9075e
Juan S. Ballen <jballen@talenta.cloud>
2023-05-09 15:36:36 -0500

    feat: searchbar interface & types

---
+++ src/components/UI/molecules/Searchbar/Searchbar.interface.ts
@@ -0,0 +1,21 @@
+import { IMainButton } from '../../atoms/MainButton'
+
+export interface ISearchbar {
+  /**
+   * Placeholder text for the searchbar
+   */
+  placeholder: string
+  /**
+   * Props for the search button
+   */
+  searchProps: IMainButton
+  /**
+   * Props for the remove/delete button
+   */
+  removeProps: IMainButton
+  /**
+   * Handler for the input value
+   */
+  onSearch: (searchValue: string) => void
+  className?: string
+}
