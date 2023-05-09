9e38c054c79de2d5c11cdd6d64ee06d0ff9a587f
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-05-09 11:32:40 -0500

    feat: the divider atom was created

---
+++ src/components/UI/atoms/Divider/Divider.interface.ts
@@ -0,0 +1,19 @@
+export interface IDivider {
+  /**
+   * You can change the width with values from 0 to 100
+   */
+  width?: number
+  /**
+   * You can change the divider color
+   */
+  color?: string
+  /**
+   * You can change the opacity with values from 0 to 10
+   */
+  opacity?: number
+  /**
+   * You can change the vertical spacing
+   */
+  spacing?: number
+  className?: string
+}
