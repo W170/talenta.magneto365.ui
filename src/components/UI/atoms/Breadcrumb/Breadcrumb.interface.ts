0b835a1d89587022a7c400eb9c35ff7cb4c5a991
Juan S. Ballen <jballen@talenta.cloud>
2023-05-10 17:37:51 -0500

    refactor: minor changes made to breadcrumb atom

--- src/components/UI/atoms/Breadcrumb/Breadcrumb.interface.ts
+++ src/components/UI/atoms/Breadcrumb/Breadcrumb.interface.ts
@@ -1,14 +1,14 @@
 export interface IBreadcrumb {
   /**
-   * Breadcrumb label text
-   */
-  label: string
-  /**
    * Possible Breadcrumb navigation URL
    */
   url?: string
   /**
-   * Breadcrumb modifier for active label
+   * Sets breadcrumb text
+   */
+  breadcrumbText: string
+  /**
+   * Sets breadcrumb modifier for active label
    */
   isActive: boolean
   className?: string
