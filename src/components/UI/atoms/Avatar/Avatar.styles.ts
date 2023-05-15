9de1a8df6b28442d987204e86c9f0ce9ec2de00d
Juan S. Ballen <jballen@talenta.cloud>
2023-05-15 09:57:27 -0500

    style: styled wrapper component created

---
+++ src/components/UI/atoms/Avatar/Avatar.styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+import { IAvatar } from './Avatar.interface'
+import React from 'react'
+
+export const withStyles = (c: React.FC<IAvatar>): React.FC<IAvatar> => styled(c)`
+  display: flex;
+  flex-direction: column;
+
+  img {
+    width: 2.25rem;
+    height: 2.25rem;
+    border-radius: 50%;
+    object-fit: cover;
+  }
+`
