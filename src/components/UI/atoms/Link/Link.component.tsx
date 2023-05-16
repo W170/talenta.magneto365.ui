4028ea74e4a3d5b05eb4c8f3f7f2aa8992f6cddb
Juan S. Ballen <jballen@talenta.cloud>
2023-05-16 08:27:27 -0500

    feat: responsiveness added to link atom

--- src/components/UI/atoms/Link/Link.component.tsx
+++ src/components/UI/atoms/Link/Link.component.tsx
@@ -2,15 +2,24 @@ import React from 'react'
 import { ILinkProps } from './Link.interface'
 import { withStyles } from './Link.styles'
 import { IconItem } from '../Icon'
+import { withContextAppProvider } from '../../../context/context.component'
+import { useMediaQuery } from '../../../hooks'
 
-const Component: React.FC<ILinkProps> = ({ href, text, iconProps, className }) => (
-  <a className={className} href={href}>
-    {iconProps && <IconItem {...iconProps} />}
-    <p>{text}</p>
-  </a>
-)
+const Component: React.FC<ILinkProps> = ({ href, text, iconProps, className }) => {
+  const linkText = useMediaQuery(text, {
+    xs: null,
+    sm: null
+  })
+
+  return (
+    <a className={className} href={href}>
+      {iconProps && <IconItem {...iconProps} />}
+      {linkText && <p>{linkText}</p>}
+    </a>
+  )
+}
 
 /**
  * Atomic UI component for Link
  */
-export const Link = withStyles(Component)
+export const Link = withContextAppProvider(withStyles(Component))
