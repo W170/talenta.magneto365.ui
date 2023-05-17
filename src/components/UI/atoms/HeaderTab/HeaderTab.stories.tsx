9433928c5a05bfd591cf783e03db9d564b8054ff
Juan S. Ballen <jballen@talenta.cloud>
2023-05-17 10:58:21 -0500

    feat: header tab styles & story

---
+++ src/components/UI/atoms/HeaderTab/HeaderTab.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { HeaderTab } from './HeaderTab.component'
+
+const meta: Meta<typeof HeaderTab> = {
+  title: 'Atoms/HeaderTab',
+  component: HeaderTab,
+  args: {
+    tabType: 'tabTitle',
+    url: '#',
+    tabText: 'Empleos:'
+  }
+}
+
+export default meta
+
+type Story = StoryObj<typeof HeaderTab>
+
+export const Default: Story = {}
+
+export const TabOption: Story = {
+  args: {
+    tabType: 'tabOption',
+    url: '#',
+    tabText: 'Sugeridos'
+  }
+}
