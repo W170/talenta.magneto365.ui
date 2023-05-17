1aef0a9e71f1449d3b08d11565ea5bc99a8d69e6
Juan S. Ballen <jballen@talenta.cloud>
2023-05-17 10:57:14 -0500

    feat: header tab component & interface

---
+++ src/components/UI/atoms/HeaderTab/HeaderTab.interface.ts
@@ -0,0 +1,17 @@
+type TabOption = 'tabTitle' | 'tabOption'
+
+export interface IHeaderTab {
+  /**
+   * Sets the type of tab
+   */
+  tabType: TabOption
+  /**
+   * Sets the url of tab
+   */
+  url: string
+  /**
+   * Sets the text of tab
+   */
+  tabText: string
+  className?: string
+}
