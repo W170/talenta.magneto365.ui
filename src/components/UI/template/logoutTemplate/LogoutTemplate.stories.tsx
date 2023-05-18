aec828c28771e2ceff29c28559e13fd50168c8bf
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-05-18 10:53:13 -0500

    feat: the properties the drawe organism was added

--- src/components/UI/template/logoutTemplate/LogoutTemplate.stories.tsx
+++ src/components/UI/template/logoutTemplate/LogoutTemplate.stories.tsx
@@ -1,11 +1,24 @@
 import { Meta, StoryObj } from '@storybook/react'
 import { LogoutTemplate } from './LogoutTemplate.component'
+import {
+  ButtonsProps,
+  ButtonsProps2,
+  ListIcon,
+  headerProps,
+  listMenuProps
+} from '../../../../constants/stories.constants'
 
 const meta: Meta<typeof LogoutTemplate> = {
   title: 'Template/Logout',
   component: LogoutTemplate,
   tags: ['autodocs'],
-  args: {}
+  args: {
+    login: ButtonsProps2,
+    createAccount: ButtonsProps,
+    listIcon: ListIcon,
+    headerProps: headerProps,
+    listMenuProps: listMenuProps
+  }
 }
 
 export default meta
