b62967784e018ead388c8bd02bdcc93863c02c08
Juan S. Ballen <jballen@talenta.cloud>
2023-05-19 10:57:38 -0500

    refactor: helper file created, interface updated & story

--- src/components/UI/organism/LogoutHeader/LogoutHeader.stories.tsx
+++ src/components/UI/organism/LogoutHeader/LogoutHeader.stories.tsx
@@ -1,31 +1,10 @@
 import { Meta, StoryObj } from '@storybook/react'
 import { LogoutHeader } from './LogoutHeader.component'
-import {
-  BreadcrumbProps,
-  LinkProps,
-  LogoProps,
-  MainButtonProps,
-  OpenButtonProps,
-  MobileSearchbarProps,
-  SearchbarProps,
-  MenuButtonProps,
-  TabProps
-} from '../../../../constants/stories.constants'
 
 const meta: Meta<typeof LogoutHeader> = {
   title: 'Organism/LogoutHeader',
   component: LogoutHeader,
-  args: {
-    openProps: OpenButtonProps,
-    tabProps: TabProps,
-    menuProps: MenuButtonProps,
-    logoProps: LogoProps,
-    mobileProps: MobileSearchbarProps,
-    searchbarProps: SearchbarProps,
-    mainButtonProps: MainButtonProps,
-    linkProps: LinkProps,
-    breadcrumbsProps: BreadcrumbProps
-  }
+  args: {}
 }
 
 export default meta
