216e779e60ce46480c28180e0eb15a267fc0f2e0
Juan S. Ballen <jballen@talenta.cloud>
2023-05-19 17:20:14 -0500

    feat: context wrapped with theme provider & exported to index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './components/UI/template/loginTemplate'
 export * from './components/UI/template/logoutTemplate'
+export * from './components/UI/organism/LogoutHeader'
 export { ContextAppProvider as MagnetoUIProvider } from './components/context/context.component'
