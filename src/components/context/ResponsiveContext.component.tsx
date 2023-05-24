d6ca92420565899422a7226bd7b867619a599659
Wilmer H Muñoz <wmunoz@talenta.cloud>
2023-05-24 08:24:16 -0500

    Revert "feat: fix with context & hook solved"
    This reverts commit 5cdeac7d9bb014f91706d5a6fedd515b73348582.

--- src/components/context/ResponsiveContext.component.tsx
+++ src/components/context/ResponsiveContext.component.tsx
@@ -1,26 +1,32 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useLayoutEffect, useRef, useState } from 'react'
 import { createContext } from 'react'
 import { Dimension } from '../../utils/eventDispatcher'
 import { ResponsiveManagement } from '../../utils/responsive/responsive.util'
 import { EventDispatcherContext } from './EventDispatcherContext.component'
 
-const responsiveManagement = new ResponsiveManagement()
-export const ResponsiveContext = createContext<{ dim: Dimension }>({ dim: responsiveManagement.getDim() })
+export const ResponsiveContext = createContext<{ dim: Dimension }>({ dim: { size: 1366, type: 'md' } })
 
 export const ResponsiveContextProvider: React.FC = ({ children }) => {
   const eventDispatcher = useContext(EventDispatcherContext)
 
-  const [dim, setDim] = useState<Dimension>(responsiveManagement.getDim())
+  const responsiveManagementRef = useRef<ResponsiveManagement>()
+  const [dim, setDim] = useState<Dimension>({ size: 1366, type: 'md' })
 
   useEffect(() => {
+    if (!responsiveManagementRef.current) return
     const unsubscribe = eventDispatcher.subscribe('UPDATE_DIMENSION', ({ dim }) => {
       setDim(dim)
     })
-    responsiveManagement.run(eventDispatcher)
+    responsiveManagementRef.current.run(eventDispatcher)
     return () => {
       unsubscribe()
     }
   }, [eventDispatcher])
+
+  useLayoutEffect(() => {
+    if (!responsiveManagementRef.current) return
+    responsiveManagementRef.current = new ResponsiveManagement()
+  }, [])
 
   return <ResponsiveContext.Provider value={{ dim }}>{children}</ResponsiveContext.Provider>
 }
