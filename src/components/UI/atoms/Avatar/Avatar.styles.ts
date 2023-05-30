53c43515fc4ebc0babc9c3e7a33cda3d2a7c0d74
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-05-30 11:46:44 -0500

    refactor: avatar udpate

--- src/components/UI/atoms/Avatar/Avatar.styles.ts
+++ src/components/UI/atoms/Avatar/Avatar.styles.ts
@@ -6,18 +6,20 @@ export const withStyles = (Component: React.FC<IAvatar>): React.FC<IAvatar> => {
   return styled(Component)`
     display: flex;
     flex-direction: column;
+    width: 2.5rem;
+    height: 2.5rem;
 
     img {
-      width: 3rem;
-      height: 3rem;
+      width: 2.5rem;
+      height: 2.5rem;
       border-radius: 50%;
       object-fit: cover;
     }
 
     .magneto-ui-default-avatar {
       display: inherit;
-      width: 3rem;
-      height: 3rem;
+      width: 2.5rem;
+      height: 2.5rem;
       justify-content: center;
       border-radius: 50%;
       background-color: ${(props) => props.theme.colors.primaryColors.green};
