de9132795ea06af2cb7146a72c8866c2acef0999
Wilmer H Muñoz <wmunoz@talenta.cloud>
2023-05-31 10:10:55 -0500

    build: rollup file loader

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
+import url from '@rollup/plugin-url'
 import postcss from 'rollup-plugin-postcss'
 import dts from 'rollup-plugin-dts'
 import packageJson from './package.json'
@@ -21,12 +22,12 @@ export default [
       }
     ],
     external: ['antd', 'react', 'axios', 'react-dom'],
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), postcss()]
+    plugins: [resolve(), commonjs(), url(), typescript({ tsconfig: './tsconfig.json' }), postcss()]
   },
   {
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [dts()],
+    plugins: [dts(), url()],
     external: [/\.(css|less|scss)$/]
   }
 ]
