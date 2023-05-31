1dd7a9c422de19b540d2fe7b8b0c5cde0b383bcb
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-05-31 16:28:22 -0500

    fix: removed properties in the storie

--- src/components/UI/molecules/MobileSearchbar/MobileSearchbar.stories.tsx
+++ src/components/UI/molecules/MobileSearchbar/MobileSearchbar.stories.tsx
@@ -1,35 +1,10 @@
-import { Add, SearchNormal1 } from 'iconsax-react'
 import { MobileSearchbar } from './MobileSearchbar.component'
 import { Meta, StoryObj } from '@storybook/react'
 
 const meta: Meta<typeof MobileSearchbar> = {
   title: 'Molecules/Mobile Searchbar',
   component: MobileSearchbar,
-  args: {
-    searchProps: {
-      buttonType: 'submit',
-      buttonText: '',
-      iconProps: {
-        Icon: SearchNormal1,
-        hover: false,
-        color: '#A3A3B5',
-        size: 16
-      },
-      buttonColor: 'transparent'
-    },
-    closeProps: {
-      buttonText: '',
-      buttonType: 'button',
-      iconProps: {
-        Icon: Add,
-        hover: false,
-        color: '#14141C',
-        size: 25,
-        variant: 'Linear'
-      },
-      buttonColor: 'transparent'
-    }
-  }
+  args: {}
 }
 
 export default meta
--- src/components/UI/molecules/Searchbar/Searchbar.stories.tsx
+++ src/components/UI/molecules/Searchbar/Searchbar.stories.tsx
@@ -1,33 +1,12 @@
 import { Meta, StoryObj } from '@storybook/react'
 import { Searchbar } from './Searchbar.component'
-import { SearchNormal1, Add } from 'iconsax-react'
 
 const meta: Meta<typeof Searchbar> = {
   title: 'Molecules/Searchbar',
   component: Searchbar,
   args: {
     placeholder: 'Busca empleos por cargo o profesion',
-    searchProps: {
-      buttonType: 'submit',
-      buttonText: '',
-      iconProps: {
-        Icon: SearchNormal1,
-        hover: false,
-        color: '#000096',
-        size: 18
-      }
-    },
-    removeProps: {
-      buttonText: '',
-      buttonType: 'button',
-      iconProps: {
-        Icon: Add,
-        hover: false,
-        color: '#14141C',
-        size: 20,
-        variant: 'Linear'
-      }
-    }
+    onSearch: () => ({})
   }
 }
 
