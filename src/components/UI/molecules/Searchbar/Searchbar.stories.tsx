19907a7ba47049ea75e0f1234fc3211d7f08e206
Juan S. Ballen <jballen@talenta.cloud>
2023-06-02 11:04:28 -0500

    refactor: searchbar button props & logic

--- src/components/UI/molecules/Searchbar/Searchbar.stories.tsx
+++ src/components/UI/molecules/Searchbar/Searchbar.stories.tsx
@@ -1,12 +1,14 @@
 import { Meta, StoryObj } from '@storybook/react'
 import { Searchbar } from './Searchbar.component'
+import { removePropsButton, searchPropsButton } from '../../../../constants/stories.constants'
 
 const meta: Meta<typeof Searchbar> = {
   title: 'Molecules/Searchbar',
   component: Searchbar,
   args: {
     placeholder: 'Busca empleos por cargo o profesion',
-    onSearch: () => ({})
+    searchButtonProps: searchPropsButton,
+    removeButtonProps: removePropsButton
   }
 }
 
