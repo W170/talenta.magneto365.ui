17fcce3e8f0dd181d3b03fc4f38e8c166fbb04cf
Juan S. Ballen <jballen@talenta.cloud>
2023-06-02 11:59:55 -0500

    refactor: stories props refactored

--- src/components/UI/molecules/Tab/Tab.stories.tsx
+++ src/components/UI/molecules/Tab/Tab.stories.tsx
@@ -1,34 +1,44 @@
 import { Meta, StoryObj } from '@storybook/react'
 import { Tab } from './Tab.component'
 
-const leftButton = {
+const leftLink = {
+  type: 'link' as const,
+  href: '#',
+  text: 'Busco Empleo',
   linkProps: {
-    type: 'link' as const,
-    href: '#',
-    text: 'Busco Empleo',
     textColor: '#000'
-  },
-  tabButtonProps: {
-    tabButtonColor: '#FFF',
-    tabButtonTextSize: '11px',
-    tabButtonTextWeight: '700',
-    boxShadow: true
   }
+}
+const leftTabButton = {
+  tabButtonColor: '#FFF',
+  tabButtonTextSize: '11px',
+  tabButtonTextWeight: '700',
+  boxShadow: true
+}
+
+const rightLink = {
+  type: 'link' as const,
+  href: '#',
+  text: 'Soy empresa',
+  linkProps: {
+    textColor: '#A3A3B5'
+  }
+}
+const rightTabButton = {
+  tabButtonColor: '#F4F4FA',
+  tabButtonTextSize: '11px',
+  tabButtonTextWeight: '400',
+  boxShadow: false
+}
+
+const leftButton = {
+  linkProps: leftLink,
+  tabButtonProps: leftTabButton
 }
 
 const rightButton = {
-  linkProps: {
-    type: 'link' as const,
-    href: '#',
-    text: 'Soy empresa',
-    textColor: '#A3A3B5'
-  },
-  tabButtonProps: {
-    tabButtonColor: '#F4F4FA',
-    tabButtonTextSize: '11px',
-    tabButtonTextWeight: '400',
-    boxShadow: false
-  }
+  linkProps: rightLink,
+  tabButtonProps: rightTabButton
 }
 
 const meta: Meta<typeof Tab> = {
