0efa3a95119361cbebbaa13c3052359c2be2fe5f
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-06-14 07:18:36 -0500

    refactor: functionality has been changed in the menu user component

--- src/components/UI/molecules/UserMenu/UserMenu.component.tsx
+++ src/components/UI/molecules/UserMenu/UserMenu.component.tsx
@@ -1,26 +1,47 @@
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { Avatar, Popover } from '../../atoms'
 import { ListMenuIcons } from '../ListMenuIcons'
 import { IMenuUser } from './UserMenu.interface'
-import { withStyles } from './UserMenu.styles'
 
 export const Component: React.FC<IMenuUser> = ({ listMenuUserProps, className, profileImage }) => {
   const [showPopover, setShowPopover] = useState(false)
+  const ref = useRef<HTMLDivElement>(null)
+
+  const handleClick = useCallback(
+    (e: MouseEvent) => {
+      if (showPopover) {
+        if (ref.current && !ref.current.contains(e.target as Node)) {
+          setShowPopover(!showPopover)
+        }
+      }
+    },
+    [showPopover]
+  )
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick)
+    return () => {
+      document.removeEventListener('click', handleClick)
+    }
+  }, [handleClick])
 
   return (
     <div className={className}>
       <Popover
         show={showPopover}
-        content={<ListMenuIcons {...listMenuUserProps} />}
+        content={
+          <div ref={ref}>
+            <ListMenuIcons {...listMenuUserProps} />
+          </div>
+        }
         width={300}
         positionX="left"
         positionY="bottom"
       >
         <Avatar {...profileImage} onClick={() => setShowPopover(!showPopover)} />
       </Popover>
-      {showPopover && <span onMouseEnter={() => setShowPopover(false)} className="magneto-ui-bghover" />}
     </div>
   )
 }
 
-export const UserMenu = withStyles(Component)
+export const UserMenu = Component
