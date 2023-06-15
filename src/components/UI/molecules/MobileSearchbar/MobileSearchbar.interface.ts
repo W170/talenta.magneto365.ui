554c8ea02c3a6150981431bd1bc662abc63160e8
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-06-15 09:46:59 -0500

    refactor: removed additional property

--- src/components/UI/molecules/MobileSearchbar/MobileSearchbar.interface.ts
+++ src/components/UI/molecules/MobileSearchbar/MobileSearchbar.interface.ts
@@ -14,7 +14,7 @@ export interface IMobileSearchbar {
   /**
    * Handler for the show mobile searchbar
    */
-  termValue?: string | string[]
+  termValue?: string
   onClick?: () => void
   className?: string
 }
