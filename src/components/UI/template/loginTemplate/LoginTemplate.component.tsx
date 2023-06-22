cb06a390631e22c82c2276d96bf14cd3900a2190
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-06-22 15:39:52 -0500

    refactor: login template was refactored

--- src/components/UI/template/loginTemplate/LoginTemplate.component.tsx
+++ src/components/UI/template/loginTemplate/LoginTemplate.component.tsx
@@ -1,31 +1,30 @@
 import React, { useState } from 'react'
 import { Drawer } from '../../molecules/Drawer'
-import { MenuContainer, withStyles } from './LoginTemplate.styles'
 import { ILoginTemplate } from './LoginTemplate.interface'
 import { ListMenuItems } from '../../molecules/ListMenuItems'
 import { ListIconLink } from '../../molecules/ListIconLink'
 import { ListIcon, logoPropsLogin } from '../../../../constants/stories.constants'
-import { ActiosContainer } from '../../organism/DrawerMenu/DrawerMenu.styles'
 import { LogoComponent } from '../../atoms/Logo'
 import { LoginHeader } from '../../organism/LoginHeader'
+import style from './loginTemplate.module.scss'
 
-const Component: React.FC<ILoginTemplate> = ({ className, listMenuProps, ...props }) => {
+const Component: React.FC<ILoginTemplate> = ({ listMenuProps, ...props }) => {
   const [toggleDrawer, setToggleDrawer] = useState(false)
 
   return (
-    <div className={className}>
+    <div className={style['magneto-ui-drawer-menu']}>
       <LoginHeader {...props} onClick={() => setToggleDrawer(true)} />
       <Drawer isOpen={toggleDrawer} onClose={() => setToggleDrawer(false)}>
         <LogoComponent {...logoPropsLogin} />
-        <MenuContainer>
+        <div className={style['menu-container']}>
           <ListMenuItems {...listMenuProps} />
-        </MenuContainer>
-        <ActiosContainer>
+        </div>
+        <div className={style.actionsContainer}>
           <ListIconLink spacing={25} listIcon={ListIcon} />
-        </ActiosContainer>
+        </div>
       </Drawer>
     </div>
   )
 }
 
-export const LoginTemplate = withStyles(Component)
+export const LoginTemplate = Component
--- src/components/UI/template/loginTemplate/LoginTemplate.interface.ts
+++ src/components/UI/template/loginTemplate/LoginTemplate.interface.ts
@@ -3,7 +3,6 @@ import { IHeaderTabs, IMobileSearchbar, ISearchbar } from '../../molecules'
 import { IListMenuItems } from '../../molecules/ListMenuItems/ListMenuItems.interface'
 import { IlistMenuUserProps } from '../../organism/LoginHeader'
 export interface ILoginTemplate {
-  className?: string
   listMenuProps: IListMenuItems
   listMenuUserProps: IlistMenuUserProps
   breadcrumbText: string
