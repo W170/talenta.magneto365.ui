ae950cb569bde0920be3de3cecc0658006a46240
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-06-22 12:12:05 -0500

    refactor: menuItem atom was refactored

--- src/components/UI/atoms/MenuItem/MenuItem.component.tsx
+++ src/components/UI/atoms/MenuItem/MenuItem.component.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { ArrowRight2 } from 'iconsax-react'
 import { IMenuItem } from './MenuItem.interface'
-import { withStyles } from './MenuItem.styles'
+import style from './menuItems.module.scss'
 
-const MenuItem: React.FC<IMenuItem> = ({ className, url = '#', text = '' }) => (
-  <div>
-    <a href={url} className={className}>
-      <ArrowRight2 color="black" />
-      <p>{text}</p>
-    </a>
-  </div>
-)
+const MenuItem: React.FC<IMenuItem> = ({ url = '#', text = '', isActive }) => {
+  const menuActive = isActive ? style['is-active'] : ''
+  return (
+    <div>
+      <a href={url} className={`${style['magneto-ui-menu-item']} ${menuActive}`}>
+        <ArrowRight2 color="black" />
+        <p>{text}</p>
+      </a>
+    </div>
+  )
+}
 
-export default withStyles(MenuItem)
+export default MenuItem
