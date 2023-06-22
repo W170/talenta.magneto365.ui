85fc294a8406a25f609f677b7a579cb84e8e1f41
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-06-22 11:03:17 -0500

    refactor: icon atom was refactored

--- src/components/UI/molecules/ListMenuIcons/ListMenuIcons.component.tsx
+++ src/components/UI/molecules/ListMenuIcons/ListMenuIcons.component.tsx
@@ -1,23 +1,16 @@
-import React, { Fragment, useContext, useMemo } from 'react'
+import React, { Fragment } from 'react'
 import { Divider } from '../../atoms'
 import { MenuIcon } from '../MenuIcon'
-import { screenSize } from '../../../../constants/responsive.constants'
-import { ResponsiveContext } from '../../../context/ResponsiveContext.component'
 import { LogoutCurve, Setting2 } from 'iconsax-react'
 import { withStyles } from './ListMenuIcons.styles'
 import { IListMenuIcons } from './ListMenuIcons.interface'
 import { useMediaQuery } from '../../../hooks'
 
 const Component: React.FC<IListMenuIcons> = ({ urlParam, menuItems, className, menuItems1440, logout, settings }) => {
-  const { dim } = useContext(ResponsiveContext)
   const { logoutText, onClick } = logout
   const { settingsText, onClick: onClickSettings } = settings
 
   const menuResponsive = useMediaQuery(menuItems1440, { lg: menuItems })
-
-  const isMobile = useMemo(() => {
-    return dim.size <= screenSize.md
-  }, [dim.size])
 
   return (
     <div className={className}>
@@ -35,7 +28,7 @@ const Component: React.FC<IListMenuIcons> = ({ urlParam, menuItems, className, m
         <MenuIcon type="button" onClick={onClickSettings} Icon={Setting2} text={settingsText} />
       </div>
 
-      <Divider spacing={isMobile ? 0 : 8} opacity={1} />
+      <Divider />
       <MenuIcon type="button" onClick={onClick} Icon={LogoutCurve} text={logoutText} />
     </div>
   )
