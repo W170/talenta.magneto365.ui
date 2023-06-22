ea6ea92ef35094920b9c212926ec4c4b2a481315
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-06-22 15:08:08 -0500

    refactor: menu icon was refactored

--- src/components/UI/molecules/MenuIcon/MenuIcon.component.tsx
+++ src/components/UI/molecules/MenuIcon/MenuIcon.component.tsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import { IconItem } from '../../atoms'
 import { IMenuIcon } from './MenuIcon.interface'
-import { withStyles } from './MenuIcon.styles'
+import style from './menuIcon.module.scss'
 
-const Component: React.FC<IMenuIcon> = ({ text, Icon, className, url, type = 'link', onClick }) => {
+const Component: React.FC<IMenuIcon> = ({ text, Icon, url, type = 'link', onClick, isActive }) => {
+  const menuActive = isActive ? style['isActive'] : ''
   return (
     <>
       {type === 'link' ? (
-        <a href={url} className={className}>
+        <a href={url} className={`${style['magneto-ui-menu-icon']} ${menuActive}`}>
           <IconItem size={20} hover={false} Icon={Icon} />
-          <p className="magneto-ui-menu-text">{text}</p>
+          <p className={style['magneto-ui-menu-text']}>{text}</p>
         </a>
       ) : (
-        <button onClick={onClick} className={className}>
+        <button onClick={onClick} className={`${style['magneto-ui-menu-icon']} ${menuActive}`}>
           <IconItem size={20} hover={false} Icon={Icon} />
-          <p className="magneto-ui-menu-text">{text}</p>
+          <p className={style['magneto-ui-menu-text']}>{text}</p>
         </button>
       )}
     </>
   )
 }
 
-export const MenuIcon = withStyles(Component)
+export const MenuIcon = Component
