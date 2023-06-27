1322bc3f91225890b2d96f5fe942a232ec863933
Juan S. Ballen <jballen@talenta.cloud>
2023-06-27 17:22:50 -0500

    feat: component styles changed to modules

--- src/components/UI/molecules/Breadcrumbs/Breadcrumbs.component.tsx
+++ src/components/UI/molecules/Breadcrumbs/Breadcrumbs.component.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Breadcrumb, IconItem } from '../../atoms'
 import { IBreadcrumbs } from './Breadcrumbs.interface'
-import { withStyles } from './Breadcrumbs.styles'
+import styles from './Breadcrumbs.modules.scss'
 import { iconPropsBreadCrumb } from '../../../../constants/stories.constants'
 
-const Component: React.FC<IBreadcrumbs> = ({ breadcrumbText, className }) => {
+const Component: React.FC<IBreadcrumbs> = ({ breadcrumbText }) => {
   return (
-    <div className={className}>
+    <div className={styles.BreadcrumbsComponent}>
       <IconItem {...iconPropsBreadCrumb} />
       <Breadcrumb breadcrumbText={breadcrumbText} />
     </div>
@@ -17,4 +17,4 @@ const Component: React.FC<IBreadcrumbs> = ({ breadcrumbText, className }) => {
  * Molecule UI component for breadcrums information
  */
 
-export const Breadcrumbs = withStyles(Component)
+export const Breadcrumbs = Component
--- src/components/UI/molecules/Breadcrumbs/Breadcrumbs.interface.ts
+++ src/components/UI/molecules/Breadcrumbs/Breadcrumbs.interface.ts
@@ -3,5 +3,4 @@ export interface IBreadcrumbs {
    * Set string that contains the breadcrumbs
    */
   breadcrumbText: string
-  className?: string
 }
