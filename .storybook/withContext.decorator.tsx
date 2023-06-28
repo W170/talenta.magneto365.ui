d3a5bfa22fe4b96c00c8e612c13e93e86cf5e987
Wilmer H Muñoz <wmunoz@talenta.cloud>
2023-06-28 17:45:01 -0500

    fix: global styles

--- .storybook/withContext.decorator.tsx
+++ .storybook/withContext.decorator.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ContextAppProvider } from '../src/components/context/context.component'
 
+const loadGoogleFonts = () => {
+  if (document.getElementById('g-fonts-mulish')) return
+  const googleFontsUri = new URL('https://fonts.googleapis.com/css2')
+  const style = document.createElement('style')
+  googleFontsUri.searchParams.set('family', 'Mulish:ital,wght@0,400;0,700;1,400;1,700')
+  googleFontsUri.searchParams.set('display', 'swap')
+  style.id = 'g-fonts-mulish'
+  style.innerHTML = `@import url('${googleFontsUri.toString()}');`
+  window.document.head.insertBefore(style, window.document.head.firstChild)
+}
+
+const fixGlobalStyles = () => {
+  if (document.getElementById('storybook-overwrites')) return
+  const style = document.createElement('style')
+  style.id = 'storybook-overwrites'
+  style.innerHTML = `
+    body {
+      box-sizing: border-box;
+      font-family: "Mulish", sans-serif, -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Lato", "Oxygen", "Ubuntu",
+      "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue";
+    }
+    ul {
+      list-style-type: none;
+      padding: 0;
+    }
+    li {
+      list-style-type: none;
+    } 
+    a {
+      text-decoration: none !important;
+      color: black;
+    }`
+  window.document.head.insertBefore(style, window.document.head.lastChild)
+}
 export const withContext = (Story: React.FC) => {
+  useEffect(loadGoogleFonts, [])
+  useEffect(fixGlobalStyles, [])
   return (
     <ContextAppProvider device="desktop">
       <Story />
