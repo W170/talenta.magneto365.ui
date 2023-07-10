0a76ecfccd6830dc718dbf8ad43faf4869085994
Juan S. Ballen <jballen@talenta.cloud>
2023-07-10 16:53:50 -0500

    refactor: component updated with changes in icons

--- src/components/UI/molecules/JobActions/JobsActions.stories.tsx
+++ src/components/UI/molecules/JobActions/JobsActions.stories.tsx
@@ -1,6 +1,6 @@
 import { StoryObj, Meta } from '@storybook/react'
 import { JobActions } from './JobActions.component'
-import { ArchiveAdd, Buildings2, ExportSquare, Share } from 'iconsax-react'
+import { ArchiveAdd, Buildings2, Export3, Share } from '../../../../constants/icons.constants'
 
 const meta: Meta<typeof JobActions> = {
   title: 'Molecules/Jobs Actions',
@@ -18,7 +18,7 @@ export const Default: Story = {
     actionsListIcon: [
       { Icon: ArchiveAdd, url: '' },
       { Icon: Share, url: '' },
-      { Icon: ExportSquare, url: '' },
+      { Icon: Export3, url: '' },
       { Icon: Buildings2, url: ' ' }
     ]
   }
