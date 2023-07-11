ce1fe9aab596504bb36ee950bccf67f56c98e2d2
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-07-11 16:12:41 -0500

    feat: containers implementation in the job page

--- src/components/UI/page/JobsPage/JobsPage.component.tsx
+++ src/components/UI/page/JobsPage/JobsPage.component.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FilterBottomHeader } from '../../template'
 import { IJobsPage } from './JobsPage.interface'
+import { JobCard } from '../../molecules/JobCard'
+import FilterContainerMenu from '../../molecules/FilterContainerMenu/FilterContainerMenu.component'
+import { classMUI } from '../../../../constants/stories.constants'
+import style from './JobsPage.module.scss'
+import { JobDetailContainer } from '../../molecules/JobDetailContainer'
 
 const JobsPage: React.FC<IJobsPage> = ({ filterBottomHeaderProps }) => {
+  const [showDetail, setShowDetail] = useState(false)
+
   return (
-    <div>
-      <FilterBottomHeader {...filterBottomHeaderProps} />
+    <div className={style[`${classMUI}-jobs-page`]}>
+      <FilterContainerMenu>
+        <p>Here should be all filters</p>
+      </FilterContainerMenu>
+      <div className={style[`${classMUI}-jobs-page--center-row`]}>
+        <FilterBottomHeader {...filterBottomHeaderProps} />
+        <div className={style[`${classMUI}-jobs-page--center-row__jobs-result`]}>
+          <JobCard />
+          <JobCard />
+          <JobCard />
+          <JobCard />
+          <button onClick={() => setShowDetail(!showDetail)}>Abre pa</button>
+        </div>
+      </div>
+      <JobDetailContainer onClose={() => setShowDetail(false)} isOpen={showDetail}>
+        <p>Detail Vacant</p>
+        <p>Here should be detail organism</p>
+      </JobDetailContainer>
     </div>
   )
 }
