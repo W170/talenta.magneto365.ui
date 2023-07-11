e6e54e2e96e0bd2adcdfa926018191e2b88a539b
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-07-11 16:14:46 -0500

    feat: children property was added

--- src/components/UI/molecules/FilterContainerMenu/FilterContainerMenu.component.tsx
+++ src/components/UI/molecules/FilterContainerMenu/FilterContainerMenu.component.tsx
@@ -3,12 +3,8 @@ import { classMUI } from '../../../../constants/stories.constants'
 import style from './FilterContainerMenu.module.scss'
 import { IFilterContainerMenu } from './FilterContainerMenu.interface'
 
-const FilterContainerMenu: React.FC<IFilterContainerMenu> = () => {
-  return (
-    <div className={style[`${classMUI}-filter-container`]}>
-      <p>Menu</p>
-    </div>
-  )
+const FilterContainerMenu: React.FC<IFilterContainerMenu> = ({ children }) => {
+  return <div className={style[`${classMUI}-filter-container`]}>{children}</div>
 }
 
 export default FilterContainerMenu
