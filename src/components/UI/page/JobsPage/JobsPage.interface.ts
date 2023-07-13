a34e759cbf4c63c36e5d3980b5ba1ac0cd60605a
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-07-13 07:54:47 -0500

    feat: jobs page was updated

--- src/components/UI/page/JobsPage/JobsPage.interface.ts
+++ src/components/UI/page/JobsPage/JobsPage.interface.ts
@@ -1,5 +1,7 @@
+import { IJobCard } from '../../molecules/JobCard/JobCard.interface'
 import { IFilterBottomHeader } from '../../template/FilterBottomHeader/FilterBottomHeader.interface'
 
 export interface IJobsPage {
   filterBottomHeaderProps: IFilterBottomHeader
+  vacantProps: IJobCard[]
 }
