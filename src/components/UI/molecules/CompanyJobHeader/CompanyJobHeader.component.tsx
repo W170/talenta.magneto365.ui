f439e39da33d2edfa457e1fbde3342f73a0997f8
Juan S. Ballen <jballen@talenta.cloud>
2023-07-14 14:52:05 -0500

    feat: new behavior added to this componet when user scrolls

--- src/components/UI/molecules/CompanyJobHeader/CompanyJobHeader.component.tsx
+++ src/components/UI/molecules/CompanyJobHeader/CompanyJobHeader.component.tsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import { CompanyLogo, JobHeader } from '../../atoms'
 import { ICompanyJobHeader } from './CompanyJobHeader.interface'
 import styles from './CompanyJobHeader.module.scss'
 
 const Component: React.FC<ICompanyJobHeader> = ({ companyLogoProps, jobHeaderProps }) => {
+  const [isHidden, setIsHidden] = useState(false)
+
+  useLayoutEffect(() => {
+    const handleInitialScroll = () => {
+      const scrollY = window.scrollY
+
+      if (scrollY === 0) {
+        setIsHidden(false)
+      } else if (!isHidden) {
+        setIsHidden(true)
+      }
+    }
+
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', handleInitialScroll)
+    }
+
+    return () => {
+      if (typeof window !== 'undefined') {
+        window.removeEventListener('scroll', handleInitialScroll)
+      }
+    }
+  }, [isHidden])
+
   return (
     <div className={styles.CompanyJobHeaderComponent}>
-      <CompanyLogo {...companyLogoProps} />
-      <JobHeader {...jobHeaderProps} />
+      <CompanyLogo {...companyLogoProps} isHidden={isHidden} />
+      <JobHeader {...jobHeaderProps} isHidden={isHidden} />
     </div>
   )
 }
