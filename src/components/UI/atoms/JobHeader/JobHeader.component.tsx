d467cc712b7eb1ef455cbbcb76b8346cfb3c94da
Juan S. Ballen <jballen@talenta.cloud>
2023-07-21 15:35:43 -0500

    refactor: headings hierarchy changed according to semantic & SEO

--- src/components/UI/atoms/JobHeader/JobHeader.component.tsx
+++ src/components/UI/atoms/JobHeader/JobHeader.component.tsx
@@ -5,8 +5,8 @@ import styles from './JobHeader.module.scss'
 const Component: React.FC<IJobHeader> = ({ jobTitle, jobSubtitle, isHidden }) => {
   return (
     <div className={styles.JobHeaderComponent}>
-      <h1>{jobTitle}</h1>
-      {!isHidden && <h2>{jobSubtitle}</h2>}
+      <h2>{jobTitle}</h2>
+      {!isHidden && <h3>{jobSubtitle}</h3>}
     </div>
   )
 }
