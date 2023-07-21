e961d4b7b8879cf905e0f22567e278aed7cb63a5
Juan S. Ballen <jballen@talenta.cloud>
2023-07-21 15:37:50 -0500

    refactor: headings hierarchy changed according to semantic & SEO, scroll logic removed

--- src/components/UI/molecules/JobCompanyHeader/JobCompanyHeader.component.tsx
+++ src/components/UI/molecules/JobCompanyHeader/JobCompanyHeader.component.tsx
@@ -1,37 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React, { useLayoutEffect, useState } from 'react'
 import { JobCompanyLogo, JobHeader } from '../../atoms'
 import { IJobCompanyHeader } from './JobCompanyHeader.interface'
 import styles from './JobCompanyHeader.module.scss'
 
 const Component: React.FC<IJobCompanyHeader> = ({ jobCompanyLogoProps, jobHeaderProps }) => {
-  const [isHidden, setIsHidden] = useState(false)
-
-  useLayoutEffect(() => {
-    const handleInitialScroll = () => {
-      const scrollY = window.scrollY
-
-      if (scrollY === 0) {
-        setIsHidden(false)
-      } else if (!isHidden) {
-        setIsHidden(true)
-      }
-    }
-
-    if (typeof window !== 'undefined') {
-      window.addEventListener('scroll', handleInitialScroll)
-    }
-
-    return () => {
-      if (typeof window !== 'undefined') {
-        window.removeEventListener('scroll', handleInitialScroll)
-      }
-    }
-  }, [isHidden])
-
   return (
     <div className={styles.JobCompanyHeaderComponent}>
-      <JobCompanyLogo {...jobCompanyLogoProps} isHidden={isHidden} />
-      <JobHeader {...jobHeaderProps} isHidden={isHidden} />
+      <JobCompanyLogo {...jobCompanyLogoProps} />
+      <JobHeader {...jobHeaderProps} />
     </div>
   )
 }
