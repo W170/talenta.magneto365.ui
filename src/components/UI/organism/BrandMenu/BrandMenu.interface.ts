54489f9d59175872a74d844277ea1f396edae616
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-07-25 16:23:52 -0500

    feat: brand menu was created

---
+++ src/components/UI/organism/BrandMenu/BrandMenu.interface.ts
@@ -0,0 +1,8 @@
+import { IBrandsContainer } from '../../molecules/BrandsContainer/BrandsContainer.interface'
+
+export interface IBrandMenu {
+  brandsProps: IBrandsContainer
+  companyUrl?: string
+  companyLogo: string
+  companySlug: string
+}
