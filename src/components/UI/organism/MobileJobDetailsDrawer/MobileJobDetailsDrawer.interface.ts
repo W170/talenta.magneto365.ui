efd34d3de39d53777756800e2b1a840268b4404e
Juan S. Ballen <jballen@talenta.cloud>
2023-07-25 10:47:56 -0500

    feat: docs comments added to interface

--- src/components/UI/organism/MobileJobDetailsDrawer/MobileJobDetailsDrawer.interface.ts
+++ src/components/UI/organism/MobileJobDetailsDrawer/MobileJobDetailsDrawer.interface.ts
@@ -6,42 +6,46 @@ import {
   IJobSkillsCard
 } from '@components/UI/molecules'
 
+/**
+ * This interface represents the props required for a mobile job details drawer component.
+ */
 export interface IMobileJobDetailsDrawer {
   /**
-   *
+   * Props for the job company logo and header section.
    */
   jobCompanyLogoProps: IJobCompanyHeader
   /**
-   *
+   * The header text to display in the job details section.
    */
   jobDetailsHeaderText: string
   /**
-   *
+   * Array of strings representing the job details to display.
    */
   jobDetailsProps: string[]
   /**
-   *
+   * Array of job detail cards to display, each card can contain different information.
    */
   jobDetailCard: IJobDetailCard[]
   /**
-   *
+   * Props for the job skills card section.
    */
   jobSkillsCardProps: IJobSkillsCard
   /**
-   *
+   * Props for the job apply card section.
    */
   jobApplyCardProps: IJobApplyCard
   /**
-   *
+   * Props for the job footer card section.
    */
   jobFooterCardProps: IJobFooterCard
   /**
-   *
+   * Represents whether the job details drawer is open or closed.
+   * (Optional property)
    */
   isOpen?: boolean
   /**
-   *
-   * @returns
+   * Callback function to be executed when the job details drawer is closed.
+   * (Optional property)
    */
   onClose?: () => void
 }
