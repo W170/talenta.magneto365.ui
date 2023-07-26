0728df487c3e7fc1c849a9babfa93bdc5412d5d2
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-07-26 10:12:04 -0500

    feat: horizontal scroll was added

--- src/components/UI/molecules/BrandsContainer/BrandsContainer.component.tsx
+++ src/components/UI/molecules/BrandsContainer/BrandsContainer.component.tsx
@@ -8,20 +8,25 @@ const Muiclass = 'magneto-ui'
 
 const BrandsContainer: React.FC<IBrandsContainer> = ({ brands, closeBrands, ourBrandsText }) => {
   return (
-    <div className={style[`${Muiclass}-brands-container`]}>
-      <div className={style[`${Muiclass}-brands-container__header`]}>
-        <p>{ourBrandsText}</p>
-        <button onClick={closeBrands} type="button">
-          <IconItem size={15} Icon={Close} />
-        </button>
+    <div className={style[`${Muiclass}-brands`]}>
+      <div className={style[`${Muiclass}-brands__header`]}>
+        <div className={style[`${Muiclass}-brands__header--row`]}>
+          <p>{ourBrandsText}</p>
+          <button onClick={closeBrands} type="button">
+            <IconItem size={15} Icon={Close} />
+          </button>
+        </div>
       </div>
-      <div className={style[`${Muiclass}-brands-container__brands`]}>
-        {brands?.map(({ brandAlt, brandImg, brandTitle, brandUrl }: IBrands, index) => (
-          <a href={brandUrl} className={style[`${Muiclass}-brands-container__brands--brand`]} key={index}>
-            <img src={brandImg} alt={brandAlt} />
-            <p>{brandTitle}</p>
-          </a>
-        ))}
+
+      <div className={style[`${Muiclass}-brands-container`]}>
+        <div className={style[`${Muiclass}-brands-container__brands`]}>
+          {brands?.map(({ brandAlt, brandImg, brandTitle, brandUrl }: IBrands, index) => (
+            <a href={brandUrl} className={style[`${Muiclass}-brands-container__brands--brand`]} key={index}>
+              <img src={brandImg} alt={brandAlt} />
+              <p>{brandTitle}</p>
+            </a>
+          ))}
+        </div>
       </div>
     </div>
   )
