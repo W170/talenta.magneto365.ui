48c464a32e1853e68817b3bb2526758711255c92
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-07-26 10:08:52 -0500

    feat: popover component was updated

--- src/components/UI/atoms/Popover/Popover.component.tsx
+++ src/components/UI/atoms/Popover/Popover.component.tsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { IPopover } from './Popover.interface'
 import style from './popover.module.scss'
 
 const Component: React.FC<IPopover> = ({ children, content, width, positionX, positionY, show, widthBase }) => {
   const showMenu = show ? 'show' : 'hidden'
+  const [hideComponent, setHideComponent] = useState(false)
+
+  useEffect(() => {
+    if (show) {
+      setHideComponent(true)
+      return
+    }
+    setTimeout(() => {
+      setHideComponent(false)
+    }, 300)
+  }, [show])
 
   return (
     <div style={{ width: widthBase }} className={style['magneto-ui-popover-container']}>
       <div
-        style={{ width: width }}
+        style={{ width: width ? width : 'auto' }}
         className={`${style['magneto-ui-popover']} ${style[positionX]} ${style[positionY]} ${style[showMenu]}`}
       >
-        {content}
+        {hideComponent && content}
       </div>
       <div className={style['magneto-ui-popover-children']}>{children}</div>
     </div>
