baff4fd7de015373d561a05d1e447eb690915e31
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-08-15 16:17:47 -0500

    feat: magneto resolution component has been created

---
+++ src/components/UI/molecules/MagnetoResolution/MagnetoResolution.component.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { MinTrabajo } from '@constants/icons.constants'
+import { classMUI } from '@constants/stories.constants'
+import style from './MagnetoResolution.module.scss'
+import { IMagnetoResolution } from './MagnetoResolution.interface'
+
+const MagnetoResolution: React.FC<IMagnetoResolution> = ({ resolutionText, resolutionHrefText, resolutionHref }) => {
+  return (
+    <div className={`${style[`${classMUI}-magneto-resolution`]}`}>
+      <img className={`${style[`${classMUI}-magneto-resolution__img`]}`} src={MinTrabajo} alt="min-trabajo-img" />
+      <p className={`${style[`${classMUI}-magneto-resolution__text`]}`}>
+        {resolutionText}
+        <a className={`${style[`${classMUI}-magneto-resolution__text--link`]}`} href={resolutionHref}>
+          {resolutionHrefText}
+        </a>
+      </p>
+    </div>
+  )
+}
+
+export default MagnetoResolution
