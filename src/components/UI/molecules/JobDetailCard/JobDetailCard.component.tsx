cf461f9c84fe435d6abd1a2a112424633f682b30
Juan S. Ballen <jballen@talenta.cloud>
2023-09-05 08:05:10 -0500

    feat: job detail card refactored, type updated to receive possible null & validation made

--- src/components/UI/molecules/JobDetailCard/JobDetailCard.component.tsx
+++ src/components/UI/molecules/JobDetailCard/JobDetailCard.component.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { IJobDetailCard } from './JobDetailCard.interface'
-
 import styles from './JobDetailCard.modules.scss'
 
 const Component: React.FC<IJobDetailCard> = ({ jobDetailCardText }) => {
   return (
     <div className={styles['magneto-ui-job-detail-card']}>
-      <div dangerouslySetInnerHTML={{ __html: jobDetailCardText }} />
+      {jobDetailCardText !== null ? (
+        <div dangerouslySetInnerHTML={{ __html: jobDetailCardText }} />
+      ) : (
+        <div>{jobDetailCardText}</div>
+      )}
     </div>
   )
 }
