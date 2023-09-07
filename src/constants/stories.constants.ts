a10b9822a1b49e5e9e87b4c5ebe0c8306a66b81e
Juan S. Ballen <jballen@talenta.cloud>
2023-09-07 12:10:40 -0500

    feat: stories constants updated with new types names

--- src/constants/stories.constants.ts
+++ src/constants/stories.constants.ts
@@ -674,8 +674,8 @@ export const vacants: IVacancies[] = [
     offerDescription:
       '<h1>¡Trabaja con el Grupo Éxito!</h1><br /><p>Importante no estar en ningun otro proceso con el Grupo Éxito. Buscamos personas como tú,  que deseen trabajar en el cargo auxiliar polivalente.</p> <h1>¿Qué necesitas?</h1><br /><p>Mínimo 2 años de experiencia, ser profesional.<br /><br />Disponibilidad de tiempo para laborar del  21 de Enero 2022  al 17 de Julio de 2023 turnos de 6  horas diarias, con posibilidad  de continuar en otras temporadas o seguir en cargos fijos.<br /><br />Estar cursando los últimos grados de la carrera o haberla finalizado.</p><h1>¿Qué te ofrecemos?</h1><br /><p>Salario entre $2’000.000 a $3’000.000 aproximadamente.</p> <h1>¡Aplica y no te pierdas esta oportunidad!</h1><br /><p>Si está interesado(a) y cumple con los requisitos debe estar pendiente de su correo electrónico y número de celular personal para envío de pruebas psicotécnicas y citación a video entrevista. FAVOR actualice esta información en la plataforma si cambió de número o el correo no es suyo o no puede ingresar.</p>',
     offerSkills: {
-      jobSkillCardHeader: 'Habilidades',
-      jobSkills: [
+      offerSkillsLabel: 'Habilidades',
+      offerSkills: [
         { id: 12, name: 'Habilidades comerciales', score: 2 },
         { id: 12, name: 'Capacidad de negociación', score: 4 },
         { id: 12, name: 'Conocimiento de microfinanzas', score: 1 }
@@ -699,8 +699,8 @@ export const vacants: IVacancies[] = [
     offerDescription:
       '<h1>¡Trabaja con el Grupo Éxito!</h1><br /><p>Importante no estar en ningun otro proceso con el Grupo Éxito. Buscamos personas como tú,  que deseen trabajar en el cargo auxiliar polivalente.</p> <h1>¿Qué necesitas?</h1><br /><p>Mínimo 2 años de experiencia, ser profesional.<br /><br />Disponibilidad de tiempo para laborar del  21 de Enero 2022  al 17 de Julio de 2023 turnos de 6  horas diarias, con posibilidad  de continuar en otras temporadas o seguir en cargos fijos.<br /><br />Estar cursando los últimos grados de la carrera o haberla finalizado.</p><h1>¿Qué te ofrecemos?</h1><br /><p>Salario entre $2’000.000 a $3’000.000 aproximadamente.</p> <h1>¡Aplica y no te pierdas esta oportunidad!</h1><br /><p>Si está interesado(a) y cumple con los requisitos debe estar pendiente de su correo electrónico y número de celular personal para envío de pruebas psicotécnicas y citación a video entrevista. FAVOR actualice esta información en la plataforma si cambió de número o el correo no es suyo o no puede ingresar.</p>',
     offerSkills: {
-      jobSkillCardHeader: 'Habilidades',
-      jobSkills: [
+      offerSkillsLabel: 'Habilidades',
+      offerSkills: [
         { id: 12, name: 'Habilidades comerciales', score: 2 },
         { id: 12, name: 'Capacidad de negociación', score: 4 },
         { id: 12, name: 'Conocimiento de microfinanzas', score: 1 }
@@ -724,8 +724,8 @@ export const vacants: IVacancies[] = [
     offerDescription:
       '<h1>¡Trabaja con el Grupo Éxito!</h1><br /><p>Importante no estar en ningun otro proceso con el Grupo Éxito. Buscamos personas como tú,  que deseen trabajar en el cargo auxiliar polivalente.</p> <h1>¿Qué necesitas?</h1><br /><p>Mínimo 2 años de experiencia, ser profesional.<br /><br />Disponibilidad de tiempo para laborar del  21 de Enero 2022  al 17 de Julio de 2023 turnos de 6  horas diarias, con posibilidad  de continuar en otras temporadas o seguir en cargos fijos.<br /><br />Estar cursando los últimos grados de la carrera o haberla finalizado.</p><h1>¿Qué te ofrecemos?</h1><br /><p>Salario entre $2’000.000 a $3’000.000 aproximadamente.</p> <h1>¡Aplica y no te pierdas esta oportunidad!</h1><br /><p>Si está interesado(a) y cumple con los requisitos debe estar pendiente de su correo electrónico y número de celular personal para envío de pruebas psicotécnicas y citación a video entrevista. FAVOR actualice esta información en la plataforma si cambió de número o el correo no es suyo o no puede ingresar.</p>',
     offerSkills: {
-      jobSkillCardHeader: 'Habilidades',
-      jobSkills: [
+      offerSkillsLabel: 'Habilidades',
+      offerSkills: [
         { id: 12, name: 'Habilidades comerciales', score: 2 },
         { id: 12, name: 'Capacidad de negociación', score: 4 },
         { id: 12, name: 'Conocimiento de microfinanzas', score: 1 }
