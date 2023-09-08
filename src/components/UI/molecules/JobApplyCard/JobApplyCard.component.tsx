fe30599b808db88a1723d338b7d8e4e788227718
Juan S. Ballen <jballen@talenta.cloud>
2023-09-08 11:13:45 -0500

    feat: new validation added for apply card if any value arrives as null/undefined

--- src/components/UI/molecules/JobApplyCard/JobApplyCard.component.tsx
+++ src/components/UI/molecules/JobApplyCard/JobApplyCard.component.tsx
@@ -14,12 +14,14 @@ const Component: React.FC<IJobApplyCard> = ({
       <div className={styles['magneto-ui-job-apply-card__wrapper']}>
         <div className={styles['magneto-ui-job-apply-card__header']}>
           <h2>{offerApplyHeader}</h2>
-          {offerApplyElements.map(({ offerApplyLabel, offerApplyInfo }: JobRequirementsElement, index: number) => (
-            <div className={styles['magneto-ui-job-apply-card__requirements']} key={index}>
-              <h3>{offerApplyLabel}</h3>
-              <p>{offerApplyInfo}</p>
-            </div>
-          ))}
+          {offerApplyElements
+            .filter(({ offerApplyInfo }) => !!offerApplyInfo)
+            .map(({ offerApplyLabel, offerApplyInfo }: JobRequirementsElement, index: number) => (
+              <div className={styles['magneto-ui-job-apply-card__requirements']} key={index}>
+                <h3>{offerApplyLabel}</h3>
+                <p>{offerApplyInfo}</p>
+              </div>
+            ))}
         </div>
         <MainButton buttonType="button" buttonText={offerApplyButtonText} onClick={onHandlerClick} />
       </div>
