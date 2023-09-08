637f006de1cc2040b8a144b6be9d4d71b8dd9409
Juan S. Ballen <jballen@talenta.cloud>
2023-09-08 11:16:06 -0500

    feat: conditional added to skill card component if skills are null/[]

--- src/components/UI/molecules/JobSkillsCard/JobSkillsCard.component.tsx
+++ src/components/UI/molecules/JobSkillsCard/JobSkillsCard.component.tsx
@@ -1,20 +1,24 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { IJobSkillsCard } from './JobSkillsCard.interface'
 import { Skill, ISkill } from '../../atoms/Skill'
 import styles from './JobSkillsCard.module.scss'
 
 const Component: React.FC<IJobSkillsCard> = ({ offerSkillsLabel, offerSkills }) => {
   return (
-    <div className={styles['magneto-ui-job-skills-card']}>
-      <h2>{offerSkillsLabel}</h2>
-      <div className={styles['magneto-ui-job-skills-card__skills-wrapper']}>
-        {offerSkills?.map(({ name, score, id }: ISkill, index: number) => (
-          <React.Fragment key={index}>
-            <Skill id={id} name={name} score={score} />
-          </React.Fragment>
-        ))}
-      </div>
-    </div>
+    <Fragment>
+      {offerSkills && (
+        <div className={styles['magneto-ui-job-skills-card']}>
+          <h2>{offerSkillsLabel}</h2>
+          <div className={styles['magneto-ui-job-skills-card__skills-wrapper']}>
+            {offerSkills?.map(({ name, score, id }: ISkill, index: number) => (
+              <React.Fragment key={index}>
+                <Skill id={id} name={name} score={score} />
+              </React.Fragment>
+            ))}
+          </div>
+        </div>
+      )}
+    </Fragment>
   )
 }
 
