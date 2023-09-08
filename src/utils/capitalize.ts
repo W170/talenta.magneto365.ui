d4d5cbb4bda167cf52baf9b7d6ca7b682664f290
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-09-08 09:19:55 -0500

    refactor: avoid uppercase error

--- src/utils/capitalize.ts
+++ src/utils/capitalize.ts
@@ -1,3 +1,3 @@
 export const capitalize = (text: string) => {
-  return text[0].toUpperCase() + text.substring(1).toLowerCase()
+  if (text) return text[0].toUpperCase() + text.substring(1).toLowerCase()
 }
