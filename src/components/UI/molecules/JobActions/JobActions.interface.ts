f1553d59f0eccaa5be7f6e6b0576cf81b826bc3d
Juan S. Ballen <jballen@talenta.cloud>
2023-09-11 16:50:50 -0500

    feat: job actions refactored, removed unnecessary elements based on implementation

--- src/components/UI/molecules/JobActions/JobActions.interface.ts
+++ src/components/UI/molecules/JobActions/JobActions.interface.ts
@@ -1,19 +1,7 @@
+import { ISaveButton } from '@components/UI/atoms/SaveButton'
 import { ReactNode } from 'react'
 
 export interface IJobsActions {
-  /**
-   * The text to be displayed on the main action button.
-   */
-  actionButtonText: string
-  /**
-   * An array of string icons to be displayed for each button in the actions section.
-   * (Optional property)
-   */
-  actionsButtonIcons?: string[]
-  /**
-   * An array of callback functions to be executed when each button in the actions section is clicked.
-   */
-  actionsButtonEvents: (() => void)[]
   /**
    * An array of string icons to be displayed for each anchor link in the actions section.
    * (Optional property)
@@ -24,11 +12,15 @@ export interface IJobsActions {
    */
   actionsAnchorLinks: string[]
   /**
-   * Callback function to be executed when the main action button is clicked.
+   *
    */
-  onHandleClick: () => void
+  externalButtonChild: ReactNode
   /**
    *
    */
-  externalChild?: ReactNode
+  externalChild: ReactNode
+  /**
+   *
+   */
+  saveButtonProps: ISaveButton
 }
--- src/components/UI/molecules/JobActions/JobsActions.stories.tsx
+++ src/components/UI/molecules/JobActions/JobsActions.stories.tsx
@@ -1,6 +1,6 @@
 import { StoryObj, Meta } from '@storybook/react'
 import { JobActions } from './JobActions.component'
-import { ArchiveAdd, Buildings2, Export3 } from '../../../../constants/icons.constants'
+import { Buildings2, Export3 } from '../../../../constants/icons.constants'
 
 const meta: Meta<typeof JobActions> = {
   title: 'Molecules/Job Actions',
@@ -12,23 +12,9 @@ export default meta
 
 type Story = StoryObj<typeof JobActions>
 
-const onApplyClick = () => {
-  console.log(1)
-}
-const onSaveClick = () => {
-  console.log(2)
-}
-const onShareClick = () => {
-  console.log(3)
-}
-
 export const Default: Story = {
   args: {
-    actionButtonText: 'Aplicar',
-    actionsButtonIcons: [ArchiveAdd],
-    actionsButtonEvents: [onSaveClick, onShareClick],
     actionsAnchorIcons: [Export3, Buildings2],
-    actionsAnchorLinks: ['link1', 'link2'],
-    onHandleClick: onApplyClick
+    actionsAnchorLinks: ['link1', 'link2']
   }
 }
