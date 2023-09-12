e7344e5f2846327eee60759cf383e5a4a8edd62b
Juan S. Ballen <jballen@talenta.cloud>
2023-09-12 12:30:53 -0500

    feat: actions molecule refactored, types & styles too

--- src/components/UI/molecules/Actions/Actions.stories.tsx
+++ src/components/UI/molecules/Actions/Actions.stories.tsx
@@ -1,7 +1,10 @@
 import { Meta, StoryObj } from '@storybook/react'
 
 import { Actions } from './Actions.component'
-import { ArchiveAdd, Buildings2, Export3, Share } from '@constants/icons.constants'
+import { Buildings2, Export3 } from '@constants/icons.constants'
+import { MainButton } from '@components/UI/atoms'
+import React from 'react'
+import { ShareButton } from '@components/UI/atoms/ShareButton'
 
 const meta: Meta<typeof Actions> = {
   title: 'Molecules/Actions',
@@ -15,13 +18,19 @@ type Story = StoryObj<typeof Actions>
 
 export const Default: Story = {
   args: {
-    ButtonText: 'Aplicar',
-    ActionsButtonIcons: [ArchiveAdd, Share],
-    ActionsButtonEvents: [],
-    ActionButtonText: ['Guardar vacante', 'Compartir'],
+    ActionsHeader: 'Comunicador audiovisual',
     ActionsAnchorIcons: [Export3, Buildings2],
     ActionsAnchorLinks: ['link1', 'link2'],
     ActionAnchorText: ['Abrir en nueva pestana', 'Ver empresa'],
-    ActionsHeader: 'Comunicador audiovisual'
+    externalChild: <ShareButton buttonTitle="share" buttonText="Compartir" />,
+    saveButtonProps: {
+      isAuthenticated: true,
+      buttonText: 'Guardar Vacante',
+      isSaved: true,
+      onClick: () => {
+        //
+      }
+    },
+    externalButtonChild: <MainButton buttonText="Aplicar" />
   }
 }
