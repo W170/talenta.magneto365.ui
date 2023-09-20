762769db46f12b92dbf13065540e1370ef9d5da8
Juan S. Ballen <jballen@talenta.cloud>
2023-09-20 11:48:07 -0500

    feat: minor changes made to detail card component

--- src/components/UI/molecules/JobDetailCard/JobDetailCard.component.tsx
+++ src/components/UI/molecules/JobDetailCard/JobDetailCard.component.tsx
@@ -1,15 +1,11 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { IJobDetailCard } from './JobDetailCard.interface'
 import styles from './JobDetailCard.modules.scss'
 
 const Component: React.FC<IJobDetailCard> = ({ offerDescription }) => {
   return (
     <div className={styles['magneto-ui-job-detail-card']}>
-      {offerDescription !== null ? (
-        <div dangerouslySetInnerHTML={{ __html: offerDescription }} />
-      ) : (
-        <div>{offerDescription}</div>
-      )}
+      {offerDescription !== null ? <div dangerouslySetInnerHTML={{ __html: offerDescription }} /> : <Fragment />}
     </div>
   )
 }
