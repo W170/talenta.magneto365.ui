1c6a58af0bb2f43fc856bb3001bf809d244c0102
Juan S. Ballen <jballen@talenta.cloud>
2023-10-26 16:21:57 -0500

    feat: new story examples added to story file regarding several scenarios

--- src/components/UI/molecules/JobDetails/JobDetails.stories.tsx
+++ src/components/UI/molecules/JobDetails/JobDetails.stories.tsx
@@ -19,8 +19,53 @@ export const Default: Story = {
       'Hace 10 días',
       '4 cupos, 256 aplicaron',
       '2 años de experiencia, profesional',
+      '$ 2.000.000 a $ 3.000.000',
+      'Bello - Copacabana - Itagui - La Estrella'
+    ],
+    iconList: [Calendar1, People, Briefcase, DollarCircle, Location],
+    altList: ['Calendar1-icon', 'People-icon', 'Briefcase-icon', 'DollarCircle-icon', 'Location-icon']
+  }
+}
+
+export const FalsyValues: Story = {
+  args: {
+    offerDetailsList: [
+      'Hace 10 días',
+      '4 cupos, 256 aplicaron',
+      null,
       undefined,
       'Bello - Copacabana - Itagui - La Estrella'
+    ],
+    iconList: [Calendar1, People, Briefcase, DollarCircle, Location],
+    altList: ['Calendar1-icon', 'People-icon', 'Briefcase-icon', 'DollarCircle-icon', 'Location-icon']
+  }
+}
+
+export const LargeData: Story = {
+  args: {
+    offerDetailsList: [
+      'Hace 9 días',
+      '9999 cupos, 7 postulantes',
+      '3 años de experiencia, no tengo estudios',
+      'Salario a convenir',
+      `Addanki - Adivivaram - Cranzalem - Adalaj - Sandur - Medak - Parichha - Otra(Colombia) - Otra(Germany) - Otra(ad-Dawhah) -  Otra(Atlantico Sur) - Otra (Atlantico Norte) - Otra(Atlnatida) - Medeiros Neto - Villa Alemana - Medellin - Ahorn - Medesano - Ahome - Medemblik - Vel'ky - Meder - Medellin de Bravo`
+    ],
+    iconList: [Calendar1, People, Briefcase, DollarCircle, Location],
+    altList: ['Calendar1-icon', 'People-icon', 'Briefcase-icon', 'DollarCircle-icon', 'Location-icon']
+  }
+}
+
+export const ExceededElements: Story = {
+  args: {
+    offerDetailsList: [
+      'Hace 9 días',
+      '9999 cupos, 7 postulantes',
+      '3 años de experiencia, no tengo estudios',
+      'Salario a convenir',
+      `Addanki - Adivivaram - Cranzalem - Adalaj - Sandur - Medak - Parichha - Otra(Colombia) - Otra(Germany) - Otra(ad-Dawhah) -  Otra(Atlantico Sur) - Otra (Atlantico Norte) - Otra(Atlnatida) - Medeiros Neto - Villa Alemana - Medellin - Ahorn - Medesano - Ahome - Medemblik - Vel'ky - Meder - Medellin de Bravo`,
+      'Elementos extra 1',
+      'Elementos extra 2',
+      'Elementos extra 3'
     ],
     iconList: [Calendar1, People, Briefcase, DollarCircle, Location],
     altList: ['Calendar1-icon', 'People-icon', 'Briefcase-icon', 'DollarCircle-icon', 'Location-icon']
