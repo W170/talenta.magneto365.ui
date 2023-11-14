3d5636d4215e45376b65e07299b4f85501715d0c
JMonsalveTL <jmonsalve@magnetoglobal.com>
2023-11-14 10:06:54 -0500

    refactor: the functions was moved to hook

--- src/components/UI/organism/MultipleSelectionEntry/MultipleSelectionEntry.component.tsx
+++ src/components/UI/organism/MultipleSelectionEntry/MultipleSelectionEntry.component.tsx
@@ -1,10 +1,11 @@
-import React, { useState, ChangeEvent, useCallback, useMemo } from 'react'
+import React from 'react'
 
 import { classMUI } from '@constants/stories'
 import style from './MultipleSelectionEntry.module.scss'
 import ContainerOptions from './children/ContainerOptions/ContainerOptions.component'
 import { withClickOut } from '@components/hoc'
-import { IListOption, IMultipleSelectionentry } from './MultipleSelectionEntry.interface'
+import { IMultipleSelectionentry } from './MultipleSelectionEntry.interface'
+import { useMultipleSelectionEntry } from './hooks/useMultipleSelectionEntry'
 
 const MultipleSelectionEntry: React.FC<IMultipleSelectionentry> = ({
   dropDownTitle,
@@ -14,56 +15,15 @@ const MultipleSelectionEntry: React.FC<IMultipleSelectionentry> = ({
   numberOfSelectable = 3,
   onChange
 }) => {
-  const valueSaved = useMemo(() => {
-    if (selectedValues) {
-      return listOptions.filter((options) => selectedValues.includes(options.id))
-    }
-  }, [listOptions, selectedValues])
-
-  const [inputValue, setInputValue] = useState('')
-  const [localSelectedValues, localSetSelectedValues] = useState<IListOption[]>(valueSaved || [])
-  const [localListOptions, localSetListOptions] = useState<IListOption[]>(listOptions)
-
-  // useEffect(() => {
-  //   if (selectedValues) onChange(selectedValues)
-  // }, [onChange, selectedValues])
-
-  const addValueToArray = useCallback(
-    (value: IListOption) => {
-      const valueExists = localSelectedValues.some((currentValues) => currentValues.id === value.id)
-      if (valueExists) return
-      if (localSelectedValues.length === numberOfSelectable) return
-      localSetSelectedValues((prevValues) => [...prevValues, value])
-      onChange([...localSelectedValues, value])
-    },
-    [localSelectedValues, numberOfSelectable, onChange]
-  )
-
-  const removeValueToArray = useCallback(
-    (value: IListOption) => {
-      const filterOptions = localSelectedValues.filter((options) => options.id !== value.id)
-      localSetSelectedValues(filterOptions)
-      onChange(filterOptions)
-    },
-    [localSelectedValues, onChange]
-  )
-
-  const handleInputChange = useCallback(
-    (event: ChangeEvent<HTMLInputElement>) => {
-      const value = event.target.value
-      setInputValue(value)
-      const filtered = listOptions.filter((option) =>
-        option.name.toLocaleLowerCase().includes(value.toLocaleLowerCase())
-      )
-      localSetListOptions(filtered)
-    },
-    [listOptions]
-  )
-
-  const clearFilter = () => {
-    localSetListOptions(listOptions)
-    setInputValue('')
-  }
+  const {
+    addValueToArray,
+    clearFilter,
+    handleInputChange,
+    inputValue,
+    localListOptions,
+    removeValueToArray,
+    localSelectedValues
+  } = useMultipleSelectionEntry({ listOptions, selectedValues, onChange, numberOfSelectable })
 
   const inputSearchProps = {
     placeholder,
