5132e756a2a67d8aefd17c496e805e8ee97d0751
WilliamCortesT <wcortes@talenta.cloud>
2023-12-20 14:42:46 -0500

    feat: add optional offer-company-name props in job-header atom

--- src/components/UI/atoms/JobHeader/JobHeader.component.tsx
+++ src/components/UI/atoms/JobHeader/JobHeader.component.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { Fragment, useMemo } from 'react'
 import { IJobHeader } from './JobHeader.interface'
 import styles from './JobHeader.module.scss'
 
-const Component: React.FC<IJobHeader> = ({ offerTitle, offerCompanyName, isHidden }) => {
+const Component: React.FC<IJobHeader> = ({ offerTitle, offerCompanyName, offerCompanyLink, isHidden }) => {
+  const showCompanyName = useMemo(() => {
+    if (isHidden || !offerCompanyName) return <Fragment />
+    if (offerCompanyLink) {
+      return (
+        <a className={styles['magneto-ui-job-header__link']} href={offerCompanyLink} title={offerCompanyName}>
+          {offerCompanyName}
+        </a>
+      )
+    }
+    return <h3 className={styles['magneto-ui-job-header__subtitle']}>{offerCompanyName}</h3>
+  }, [isHidden, offerCompanyLink, offerCompanyName])
+
   return (
     <div className={styles['magneto-ui-job-header']}>
       <h2 className={styles['magneto-ui-job-header__title']}>{offerTitle}</h2>
-      {!isHidden && <h3 className={styles['magneto-ui-job-header__subtitle']}>{offerCompanyName}</h3>}
+      {showCompanyName}
     </div>
   )
 }
