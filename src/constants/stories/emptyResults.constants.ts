d2c73831212c9bb862b4a1c0bf834a9c5c3f5e08
Juan S. Ballen <jballen@talenta.cloud>
2024-02-20 11:46:52 -0500

    feat: new values added to constant values

--- src/constants/stories/emptyResults.constants.ts
+++ src/constants/stories/emptyResults.constants.ts
@@ -1,9 +1,9 @@
-import { IEmptyResutls } from '@components/UI/molecules/EmptyResults/EmptyResults.interface'
+import { IEmptyResults } from '@components/UI/molecules/EmptyResults/EmptyResults.interface'
 
-export const emptyResultsProps: IEmptyResutls = {
+export const emptyResultsProps: IEmptyResults = {
   noResultsImage:
     'https://www.magneto365.com/co/_next/image?url=https%3A%2F%2Fstatic.magneto365.com%2Fco%2F_next%2Fstatic%2Fmedia%2FemptyResultWithSession.6de8a42c.png&w=256&q=75',
-  titleNoResutls:
+  titleNoResults:
     'Actualmente no encontramos vacantes, pero puedes realizar una nueva búsqueda o acceder a otras secciones: ',
   noResultsDescription:
     'Modifica los términos de búsqueda o filtros que usaste, utiliza cargos o profesiones similares.',
@@ -15,5 +15,11 @@ export const emptyResultsProps: IEmptyResutls = {
     { tag: 'Empleos más buscados', url: '#' },
     { tag: 'Volver al inicio', url: '#' },
     { tag: 'Blog', url: '#' }
-  ]
+  ],
+  noResultsMagnetoBusinessOne: '¿Tienes ofertas de empleo para publicar? Visita ',
+  noResultsMagnetoBusinessTwo: ' y publica ilimitadamente.',
+  magnetoBusiness: {
+    magnetoLink: '#',
+    magnetoText: 'Magneto Negocios'
+  }
 }
