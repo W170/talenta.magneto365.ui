838a93eaa5f5e30e3093cffd6dd68c3c83ad24df
Santiago Granada <sgranada@talenta.cloud>
2024-03-13 11:07:50 -0500

    feat: add infinity scroll functionality

--- src/components/UI/page/SuggestedJobsPage/SuggestedJobsPage.component.tsx
+++ src/components/UI/page/SuggestedJobsPage/SuggestedJobsPage.component.tsx
@@ -4,7 +4,6 @@ import { JobCardSkeleton } from '@components/UI/molecules/JobCard/children'
 import { JobDetailsDrawer, MobileJobDetailsDrawer } from '@components/UI/organism'
 import { useMediaQuery } from '@components/hooks'
 import { classMUI } from '@constants/stories'
-import { showDetailByWindow } from '../JobsPage/utils'
 import { ISuggestedJobsPage } from './SuggestedJobsPage.interface'
 import style from './SuggestedJobsPage.module.scss'
 
@@ -16,12 +15,61 @@ const Component: React.FC<ISuggestedJobsPage> = ({
   jobDetailsDrawerProps,
   jobSelected,
   mobileJobDetailsDrawerProps,
+  paginationProps,
   setJobSelected,
   vacantProps
 }) => {
   const [isBottomPage, setIsBottomPage] = useState<boolean>(false)
   const [showDetail, setShowDetail] = useState(device === 'desktop')
   const [selectedJobId, setSelectedJobId] = useState<number | null>(null)
+
+  const {
+    total: totalVacants,
+    onChange: onChangePage,
+    current: currentPage,
+    pageSize
+  } = useMemo(() => paginationProps, [paginationProps])
+
+  const hasVacancies = useMemo(() => !!vacantProps?.length || isLoading, [isLoading, vacantProps?.length])
+  const vacantsLength = useMemo(() => vacantProps.length, [vacantProps.length])
+  const fetchMoredata = useMemo(
+    () => isBottomPage && !isLoading && totalVacants > vacantsLength && pageSize * currentPage == vacantsLength,
+    [currentPage, isBottomPage, isLoading, pageSize, totalVacants, vacantsLength]
+  )
+  const cardsAltRender = useMemo(() => {
+    if (isLoading) {
+      return currentPage == 1 ? <JobCardSkeleton numCard={20} /> : null
+    }
+
+    return <EmptyResults {...emptyResultsProps} />
+  }, [isLoading, emptyResultsProps, currentPage])
+
+  useEffect(() => {
+    const element = document.getElementsByTagName('body')[0]
+    element.style.overflowY = 'hidden'
+    return () => {
+      element.style.overflowY = 'auto'
+    }
+  }, [])
+
+  useEffect(() => {
+    if (!fetchMoredata) return
+    onChangePage(currentPage + 1, pageSize)
+  }, [currentPage, fetchMoredata, onChangePage, pageSize, paginationProps])
+
+  useEffect(() => {
+    const element = document.getElementById('magneto-ui-suggestedJobs-page')
+    if (!element) return
+    const handleScroll = () => {
+      const { scrollTop, clientHeight, scrollHeight } = element
+      const isBottom = scrollTop + clientHeight >= scrollHeight
+      if (isBottomPage != isBottom) setIsBottomPage(isBottom)
+    }
+    element.addEventListener('scroll', handleScroll)
+    return () => {
+      element.removeEventListener('scroll', handleScroll)
+    }
+  }, [isBottomPage])
 
   const handleVacant = useCallback(
     (id: number | null) => {
@@ -34,58 +82,17 @@ const Component: React.FC<ISuggestedJobsPage> = ({
     [setJobSelected]
   )
 
-  useEffect(() => {
-    const element = document.getElementsByTagName('body')[0]
-    element.style.overflowY = 'hidden'
-    return () => {
-      element.style.overflowY = 'auto'
-    }
-  }, [])
-
-  useEffect(() => {
-    console.log('Is bottom page: ', isBottomPage)
-  }, [isBottomPage])
-
-  useEffect(() => {
-    const element = document.getElementById('magneto-ui-suggestedJobs-page')
-    if (!element) return
-    const handleScroll = () => {
-      const { scrollTop, clientHeight, scrollHeight } = element
-      const isBottom = scrollTop + clientHeight >= scrollHeight
-      setIsBottomPage(isBottom)
-    }
-    element.addEventListener('scroll', handleScroll)
-    return () => {
-      element.removeEventListener('scroll', handleScroll)
-    }
-  }, [])
-
-  const handleJobCardClick = (id: number | null) => {
-    setSelectedJobId(id)
-    handleVacant(id)
-  }
-
-  useEffect(() => {
-    setShowDetail(showDetailByWindow('magneto-ui-suggestedJobs-page'))
-  }, [])
-
   const onClose = useCallback(() => {
     setShowDetail(false)
     setJobSelected(null)
   }, [setJobSelected])
-
-  const hasVacancies = useMemo(() => !!vacantProps?.length || isLoading, [isLoading, vacantProps?.length])
 
   const JobDetailsDrawerComponent = useMediaQuery(
     <JobDetailContainer onClose={onClose} isOpen={showDetail && hasVacancies}>
       {jobDetailAction ? (
         jobDetailAction
       ) : (
-        <JobDetailsDrawer
-          {...jobDetailsDrawerProps}
-          isLoading={isLoading || !jobSelected}
-          selectedJobId={selectedJobId}
-        />
+        <JobDetailsDrawer {...jobDetailsDrawerProps} isLoading={!jobSelected} selectedJobId={selectedJobId} />
       )}
     </JobDetailContainer>,
     {
@@ -95,37 +102,37 @@ const Component: React.FC<ISuggestedJobsPage> = ({
           onClose={onClose}
           isOpen={showDetail}
           jobDetailAction={jobDetailAction}
-          isLoading={isLoading || !jobSelected}
+          isLoading={!jobSelected}
         />
       )
     }
   )
 
-  const cardsAltRender = useMemo(() => {
-    if (isLoading) {
-      return <JobCardSkeleton numCard={20} />
-    }
-
-    return <EmptyResults {...emptyResultsProps} />
-  }, [isLoading, emptyResultsProps])
+  const handleJobCardClick = (id: number | null) => {
+    setSelectedJobId(id)
+    handleVacant(id)
+  }
 
   return (
     <Fragment>
       <div id="magneto-ui-suggestedJobs-page" className={style[`${classMUI}-suggestedJobs-page`]}>
         <div className={style[`${classMUI}-suggestedJobs-page--center-row`]}>
           <div className={style[`${classMUI}-suggestedJobs-page--center-row__jobs-result`]}>
-            {vacantProps.length <= 0 || isLoading
+            {vacantProps.length <= 0 || (isLoading && currentPage == 1)
               ? cardsAltRender
               : vacantProps.map(({ id, ...props }) => (
-                  <JobCard
-                    isLoading={isLoading}
-                    isActive={id === jobSelected?.id}
-                    id={id}
-                    showDetail={() => handleJobCardClick(id)}
-                    key={`${id}-JobsPage`}
-                    {...props}
-                  />
+                  <>
+                    <JobCard
+                      isLoading={isLoading}
+                      isActive={id === jobSelected?.id}
+                      id={id}
+                      showDetail={() => handleJobCardClick(id)}
+                      key={`${id}-JobsPage`}
+                      {...props}
+                    />
+                  </>
                 ))}
+            {isLoading && currentPage > 1 && <JobCardSkeleton numCard={1} goToBottom />}
           </div>
         </div>
         <div className={style[`${classMUI}-suggestedJobs-page__jobs-detail`]}>{JobDetailsDrawerComponent}</div>
