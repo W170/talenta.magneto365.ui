a85299ad44f7113e99f987f71eb93b5a4bc01ef1
Daniel Morales <dmorales@talenta.cloud>
2024-04-24 13:39:07 -0500

    feat: removed site map texts

--- src/components/UI/molecules/RightsReservedText/RightsReservedText.component.tsx
+++ src/components/UI/molecules/RightsReservedText/RightsReservedText.component.tsx
@@ -3,14 +3,10 @@ import { classMUI } from '@constants/stories'
 import style from './RightsReservedText.module.scss'
 import { IRightsReservedText } from './RightsReservedText.interface'
 
-const RightsReservedText: React.FC<IRightsReservedText> = ({ magnetoRightsText, mapSiteHref, mapSiteText }) => {
+const RightsReservedText: React.FC<IRightsReservedText> = ({ magnetoRightsText }) => {
   return (
     <div className={`${style[`${classMUI}-rights-reserved`]}`}>
       <p>{magnetoRightsText}</p>
-      <span>|</span>
-      <a className={`${style[`${classMUI}-rights-reserved__link`]}`} href={mapSiteHref}>
-        {mapSiteText}
-      </a>
     </div>
   )
 }
