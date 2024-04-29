48c87c5cbd4beaac593d993717e42afb4876118c
Juan S. Ballen <jballen@talenta.cloud>
2024-04-29 15:19:42 -0500

    feat: state added for selected option, plus the onClick handler plus more funct

--- src/components/UI/molecules/DateSelection/DateSelection.component.tsx
+++ src/components/UI/molecules/DateSelection/DateSelection.component.tsx
@@ -1,21 +1,45 @@
-import React from 'react';
-import {DateOptions} from "@components/UI/atoms/DateOptions";
-import {MobileDrawer} from "@components/UI/molecules";
-import {IDateSelection} from "@components/UI/molecules/DateSelection/DateSelection.interface";
-import styles from "./DateSelection.module.scss";
+import React, { useState } from 'react'
+import { DateOptions } from '@components/UI/atoms/DateOptions'
+import { MobileDrawer } from '@components/UI/molecules'
+import { IDateSelection } from '@components/UI/molecules/DateSelection/DateSelection.interface'
+import styles from './DateSelection.module.scss'
 
-const Component:React.FC<IDateSelection> = ({isOpen, onClose, selectionHeader, cancelSelection, applySelection,  dateOptions, onClick}) => {
+const Component: React.FC<IDateSelection> = ({
+  isOpen,
+  onClose,
+  selectionHeader,
+  cancelSelection,
+  applySelection,
+  dateOptions
+}) => {
+  const [selectedOption, setSelectedOption] = useState<string | number | null>(null)
+
+  const handleOptionClick = (optionValue: string | number) => {
+    setSelectedOption(optionValue)
+  }
+
+  const onCancelClick = () => {
+    console.log('Cierro')
+  }
+  const onApplyClick = () => {
+    console.log('Apply')
+  }
+
   return (
     <MobileDrawer isOpen={isOpen} onClose={onClose}>
       <div className={styles['magneto-ui--date-selection__header']}>
         <h2>{selectionHeader}</h2>
       </div>
       <div className={styles['magneto-ui--date-selection__options']}>
-        <DateOptions optionsList={dateOptions} handleOnClick={onClick} />
+        <DateOptions handleOnClick={handleOptionClick} optionsList={dateOptions} selected={selectedOption} />
       </div>
       <div className={styles['magneto-ui--date-selection__btn--wrapper']}>
-        <button className={styles['magneto-ui--date-selection__cancel-btn']}>{cancelSelection}</button>
-        <button className={styles['magneto-ui--date-selection__apply-btn']}>{applySelection}</button>
+        <button className={styles['magneto-ui--date-selection__cancel-btn']} onClick={onCancelClick}>
+          {cancelSelection}
+        </button>
+        <button className={styles['magneto-ui--date-selection__apply-btn']} onClick={onApplyClick}>
+          {applySelection}
+        </button>
       </div>
     </MobileDrawer>
   )
