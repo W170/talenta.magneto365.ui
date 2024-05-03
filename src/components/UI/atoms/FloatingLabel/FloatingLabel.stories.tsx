816d7467798b075eb9bc43ac605baef38349ef15
Juan S. Ballen <jballen@talenta.cloud>
2024-05-03 13:31:29 -0500

    feat: story file created for new component

---
+++ src/components/UI/atoms/FloatingLabel/FloatingLabel.stories.tsx
@@ -0,0 +1,22 @@
+import React, { ChangeEvent } from 'react'
+import { Meta, StoryObj } from '@storybook/react'
+import { FloatingLabel } from '@components/UI/atoms/FloatingLabel/FloatingLabel.component'
+import { Input } from '@components/UI/atoms'
+
+const meta: Meta<typeof FloatingLabel> = {
+  title: 'Atoms/ FloatingLabel',
+  component: FloatingLabel,
+  args: {
+    label: 'Escribe tu fecha de nacimiento',
+    value: '',
+    children: (
+      <Input value="" onChange={(e: ChangeEvent<HTMLInputElement>) => e.target.value} type="email" placeholder="" />
+    )
+  }
+}
+
+export default meta
+
+type Story = StoryObj<typeof FloatingLabel>
+
+export const Default: Story = {}
