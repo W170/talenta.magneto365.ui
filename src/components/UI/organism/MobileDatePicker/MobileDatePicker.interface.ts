707e5e2fd6dadd4dc98d1d04164bd4887b2d2ac3
Juan S. Ballen <jballen@talenta.cloud>
2024-05-15 15:16:21 -0500

    feat: new data types added to component interface

--- src/components/UI/organism/MobileDatePicker/MobileDatePicker.interface.ts
+++ src/components/UI/organism/MobileDatePicker/MobileDatePicker.interface.ts
@@ -1,13 +1,35 @@
 import { IOptionValues } from '@components/UI/atoms'
 
 export interface IMobileDatePicker {
+  /*
+   */
   applyLabel: string
-
+  /*
+   */
   cancelLabel: string
-
+  /*
+   */
+  monthsLabels: string[]
+  /*
+   */
   selectionHeader: string
-
-  dateOptions: IOptionValues[]
-
+  /*
+   */
+  monthDateOptions: IOptionValues[]
+  /*
+   */
+  yearDateOptions: IOptionValues[]
+  /*
+   */
+  monthPlaceholder: string
+  /*
+   */
+  yearPlaceholder: string
+  /*
+   */
   disabled?: boolean
+  /**
+   * Callback function to be executed when the selected date in the date picker changes.
+   */
+  onChange: (value: string | Date | null) => void
 }
