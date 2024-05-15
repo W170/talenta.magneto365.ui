05d1fab1b81369a7111d09af7b8a7d8a33bf9408
Juan S. Ballen <jballen@talenta.cloud>
2024-05-15 15:14:37 -0500

    feat: new props, states, variables plus more behavior added to component to work according to req

--- src/components/UI/organism/MobileDatePicker/MobileDatePicker.component.tsx
+++ src/components/UI/organism/MobileDatePicker/MobileDatePicker.component.tsx
@@ -6,72 +6,133 @@ import { IMobileDatePicker } from '@components/UI/organism/MobileDatePicker/Mobi
 import styles from './MobileDatePicker.module.scss'
 
 const Component: React.FC<IMobileDatePicker> = ({
-  dateOptions,
-  disabled,
-  selectionHeader,
+  applyLabel,
   cancelLabel,
-  applyLabel
+  disabled,
+  monthsLabels,
+  monthDateOptions,
+  yearDateOptions,
+  monthPlaceholder,
+  yearPlaceholder,
+  onChange,
+  selectionHeader
 }) => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isMonthPickerOpen, setIsMonthPickerOpen] = useState(false)
+  const [isYearPickerOpen, setIsYearPickerOpen] = useState(false)
 
   const [monthSelected, setMonthSelected] = useState<string | number | null>()
   const [yearSelected, setYearSelected] = useState<string | number | null>()
 
-  const toggleIsOpen = useCallback(() => {
-    setIsOpen(true)
-  }, [])
+  const FIRST_OF_MONTH = 1
+
+  const getMonthName = (monthIndex: string | number | undefined) => {
+    return monthsLabels[monthIndex as number]
+  }
+
+  const toggleMonthPicker = useCallback(() => {
+    if (!disabled) {
+      setIsMonthPickerOpen(true)
+    }
+  }, [disabled])
+
+  const toggleYearPicker = useCallback(() => {
+    if (!disabled) {
+      setIsYearPickerOpen(true)
+    }
+  }, [disabled])
 
   const onClose = useCallback(() => {
-    setIsOpen(false)
+    setIsMonthPickerOpen(false)
+    setIsYearPickerOpen(false)
   }, [])
 
   const disabledArrow = useMemo(() => {
-    if (disabled)
-      return (
-        <IconItem
-          className={styles['magneto-ui--mobile-date-picker__icon']}
-          icon={ArrowDown2}
-          alt="arrow-icon"
-          size={24}
-        />
-      )
+    if (disabled) return <IconItem icon={ArrowDown2} alt="arrow-icon" size={24} />
     return <IconItem icon={ArrowDown3} alt="arrow-icon" size={24} />
   }, [disabled])
 
-  const handleDateSelection = (selectedOption: string | number | null) => {
-    setMonthSelected(selectedOption)
-    setYearSelected(selectedOption)
+  const handleMonthDateSelection = (monthSelected: string | number | null) => {
+    setMonthSelected(monthSelected)
+    if (yearSelected != '') {
+      onChange(new Date(Number(yearSelected), Number(monthSelected), FIRST_OF_MONTH))
+    }
+  }
+
+  const handleYearDateSelection = (yearSelected: string | number | null) => {
+    setYearSelected(yearSelected)
+    if (monthSelected != '') {
+      onChange(new Date(Number(yearSelected), Number(monthSelected), FIRST_OF_MONTH))
+    }
   }
 
   return (
     <section className={styles['magneto-ui--mobile-date-picker']}>
       <div
-        className={`${styles['magneto-ui--mobile-date-picker__container']} ${
-          monthSelected ? styles['placeholder-shrink'] : ''
-        }`}
-        onClick={toggleIsOpen}
+        className={
+          styles[
+            `${
+              disabled
+                ? 'magneto-ui--mobile-date-picker__container--disabled'
+                : 'magneto-ui--mobile-date-picker__container'
+            }`
+          ]
+        }
+        onClick={toggleMonthPicker}
       >
-        <span className={`${styles['placeholder-text']} ${monthSelected ? styles['placeholder-up'] : ''}`}>
-          {'Mes'}
+        <span className={`${styles['placeholder-text']}`}>{monthPlaceholder}</span>
+        {monthSelected !== null ? getMonthName(monthSelected) : ''}
+        <span
+          className={
+            styles[
+              `${disabled ? 'magneto-ui--mobile-date-picker__icon--disabled' : 'magneto-ui--mobile-date-picker__icon'}`
+            ]
+          }
+        >
+          {disabledArrow}
         </span>
-        {monthSelected}
-        {yearSelected}
-        <span className={styles['magneto-ui--mobile-date-picker__icon']}>{disabledArrow}</span>
       </div>
       <DateSelection
         applyLabel={applyLabel}
         cancelLabel={cancelLabel}
-        dateOptions={dateOptions}
-        isOpen={isOpen}
+        dateOptions={monthDateOptions}
+        isOpen={isMonthPickerOpen}
         selectionHeader={selectionHeader}
         onClose={onClose}
-        onApplyCallback={handleDateSelection}
+        onApplyCallback={handleMonthDateSelection}
       />
-      <div className={styles['magneto-ui--mobile-date-picker__container']}>
+      <div
+        className={
+          styles[
+            `${
+              disabled
+                ? 'magneto-ui--mobile-date-picker__container--disabled'
+                : 'magneto-ui--mobile-date-picker__container'
+            }`
+          ]
+        }
+        onClick={toggleYearPicker}
+      >
+        <span className={`${styles['placeholder-text']}`}>{yearPlaceholder}</span>
         {yearSelected}
-        2021
-        <span className={styles['magneto-ui--mobile-date-picker__icon']}>{disabledArrow}</span>
+        <span
+          className={
+            styles[
+              `${disabled ? 'magneto-ui--mobile-date-picker__icon--disabled' : 'magneto-ui--mobile-date-picker__icon'}`
+            ]
+          }
+        >
+          {disabledArrow}
+        </span>
       </div>
+      <DateSelection
+        applyLabel={applyLabel}
+        cancelLabel={cancelLabel}
+        dateOptions={yearDateOptions}
+        isOpen={isYearPickerOpen}
+        selectionHeader={selectionHeader}
+        onClose={onClose}
+        onApplyCallback={handleYearDateSelection}
+      />
     </section>
   )
 }
