62d655093f598402c4f993223d6e3a8f1d756bf1
Juan S. Ballen <jballen@talenta.cloud>
2024-05-15 15:20:30 -0500

    feat: new props added, plus another story obj for disabled alternative

--- src/components/UI/organism/MobileDatePicker/MobileDatePicker.stories.tsx
+++ src/components/UI/organism/MobileDatePicker/MobileDatePicker.stories.tsx
@@ -2,7 +2,7 @@ import { Meta, StoryObj } from '@storybook/react'
 
 import { MobileDatePicker } from '@components/UI/organism/MobileDatePicker/MobileDatePicker.component'
 
-import { monthOptionsValue, months } from '@constants/stories/DatePicker.constants'
+import { monthOptionsValue, months, yearOptionsLabel, yearOptionsValue } from '@constants/stories/DatePicker.constants'
 
 const storyMonths = []
 
@@ -10,13 +10,23 @@ for (let i = 0; i < Math.min(monthOptionsValue.length, months.length); i++) {
   storyMonths.push({ optionValue: monthOptionsValue[i], optionLabel: months[i] })
 }
 
+const storyYears = []
+
+for (let i = 0; i < Math.min(yearOptionsValue.length, yearOptionsLabel.length); i++) {
+  storyYears.push({ optionValue: yearOptionsValue[i], optionLabel: yearOptionsLabel[i] })
+}
+
 const meta: Meta<typeof MobileDatePicker> = {
-  title: 'Molecules/ Mobile DatePicker',
+  title: 'Organism/ Mobile DatePicker',
   component: MobileDatePicker,
   args: {
     applyLabel: 'Seleccionar',
     cancelLabel: 'Cerrar',
-    dateOptions: storyMonths,
+    monthsLabels: months,
+    monthDateOptions: storyMonths,
+    yearDateOptions: storyYears,
+    monthPlaceholder: 'Month',
+    yearPlaceholder: 'Year',
     selectionHeader: 'Selecciona un mes'
   }
 }
@@ -26,3 +36,10 @@ export default meta
 type Story = StoryObj<typeof MobileDatePicker>
 
 export const Default: Story = {}
+
+export const Disabled: Story = {
+  args: {
+    ...Default.args,
+    disabled: true
+  }
+}
