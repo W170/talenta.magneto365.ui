05a6df92538066c9e6fbddac0fa0b99dcf08e2bf
luis-alfonso-dsg <lamarin@magnetoglobal.com>
2024-05-17 11:28:17 -0500

    perf: improve Radio performance

--- src/components/UI/atoms/Radio/Radio.component.tsx
+++ src/components/UI/atoms/Radio/Radio.component.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { ERadioType, ComponentProps } from './Radio.interface'
 import { getRadioComponent } from './Radio.constants'
 
 const Component = <T extends keyof typeof ERadioType>({ type, ...props }: { type: T } & ComponentProps<T>) => {
-  const RadioType = getRadioComponent(ERadioType[type ?? 'radio'])
+  const RadioType = useMemo(() => getRadioComponent(ERadioType[type ?? 'radio']), [type])
 
   if (!RadioType) return null
 
