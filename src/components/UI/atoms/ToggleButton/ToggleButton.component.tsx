178baf4a79581564063c9f59d4774b80163b4c85
JMonsalveTL <jmonsalve@magnetoglobal.com>
2024-06-14 14:30:27 -0500

    feat: toggle button component was created

---
+++ src/components/UI/atoms/ToggleButton/ToggleButton.component.tsx
@@ -0,0 +1,54 @@
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import { IToggleButton } from './ToggleButton.interface'
+import { classMUI } from '@constants/stories'
+import styles from './ToggleButton.module.scss'
+
+const Component: React.FC<IToggleButton> = ({ className, color, onChange, id, name, defaultValue }) => {
+  const [isSelected, setIsSelected] = useState<string>('')
+
+  useEffect(() => {
+    if (defaultValue && defaultValue.includes(id.toString())) {
+      setIsSelected(defaultValue)
+    }
+  }, [defaultValue, id])
+
+  const handleClick = useCallback(
+    ({ id, name }) => {
+      if (isSelected) {
+        setIsSelected('')
+        onChange({ id, name: undefined })
+        return
+      }
+      onChange({ id, name })
+      setIsSelected(id.toString())
+    },
+    [isSelected, onChange]
+  )
+
+  const selectStyles = useMemo(() => {
+    if (isSelected) {
+      return {
+        backgroundColor: color || '#F0F1F3'
+      }
+    }
+    return {
+      backgroundColor: 'transparent',
+      border: `${color || '#E3E7E9'} solid 2px`
+    }
+  }, [color, isSelected])
+
+  return (
+    <button
+      className={`${styles[`${classMUI}-toggle-button`]} ${className || ''} ${
+        isSelected ? styles['toggle-button-selected'] : ''
+      }`}
+      style={{ ...selectStyles }}
+      onClick={() => handleClick({ id, name })}
+      type="button"
+    >
+      {name}
+    </button>
+  )
+}
+
+export const ToggleButton = Component
