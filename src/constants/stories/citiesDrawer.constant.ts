0cad0eaafcff6bf0749e5778545f3f07420910c6
Daniel Morales <dmorales@talenta.cloud>
2024-06-17 10:47:32 -0500

    feat: update props and interface on cities detail drawer

--- src/constants/stories/citiesDrawer.constant.ts
+++ src/constants/stories/citiesDrawer.constant.ts
@@ -16,3 +16,25 @@ export const CitiesDetailDrawerProps: ICitiesDetailDrawer = {
   isRemote: true,
   textRemote: 'Remoto / híbrido en: '
 }
+
+export const CitiesDetailDrawerPropsWithoutUrl: ICitiesDetailDrawer = {
+  cities: [
+    {
+      name: 'Bello',
+      url: 'https://www-stg.magneto365.com/co/trabajos/ofertas-empleo-en-bello',
+      title: 'Ofertas empleo en Bello'
+    },
+    {
+      name: 'Copacabana',
+      url: 'https://www-stg.magneto365.com/co/trabajos/ofertas-empleo-en-copacabana',
+      title: 'Ofertas empleo en Copacabana'
+    },
+    {
+      name: 'Candelaria',
+      url: null,
+      title: 'Ofertas empleo en Candelaria'
+    }
+  ],
+  isRemote: true,
+  textRemote: 'Remoto / híbrido en: '
+}
