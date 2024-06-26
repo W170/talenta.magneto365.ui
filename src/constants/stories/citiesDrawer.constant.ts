80d6e9b8856ee2bd740de389b409c1b6bc91fa61
Daniel Morales <dmorales@talenta.cloud>
2024-06-26 08:11:14 -0500

    fix: fixed bug 32596 styles on large data cities

--- src/constants/stories/citiesDrawer.constant.ts
+++ src/constants/stories/citiesDrawer.constant.ts
@@ -17,6 +17,108 @@ export const CitiesDetailDrawerProps: ICitiesDetailDrawer = {
   textRemote: 'Remoto / híbrido en: '
 }
 
+export const CitiesLargeDetailDrawerProps: ICitiesDetailDrawer = {
+  cities: [
+    {
+      name: 'Bello',
+      url: 'https://www-stg.magneto365.com/co/trabajos/ofertas-empleo-en-bello',
+      title: 'Ofertas empleo en Bello'
+    },
+    {
+      name: 'Copacabana',
+      url: 'https://www-stg.magneto365.com/co/trabajos/ofertas-empleo-en-copacabana',
+      title: 'Ofertas empleo en Copacabana'
+    },
+    {
+      name: 'Cali',
+      url: 'https://www-stg.magneto365.com/co/trabajos/ofertas-empleo-en-Cali',
+      title: 'Ofertas empleo en Cali'
+    },
+    {
+      name: 'Medellin',
+      url: 'https://www-stg.magneto365.com/co/trabajos/ofertas-empleo-en-medellin',
+      title: 'Ofertas empleo en medellin'
+    },
+    {
+      name: 'Bogota',
+      url: 'https://www-stg.magneto365.com/co/trabajos/ofertas-empleo-en-bogota',
+      title: 'Ofertas empleo en Bogota'
+    },
+    {
+      name: 'Monteria',
+      url: 'https://www-stg.magneto365.com/co/trabajos/ofertas-empleo-en-monteria',
+      title: 'Ofertas empleo en Monteria'
+    },
+    {
+      name: 'Popayan',
+      url: 'https://www-stg.magneto365.com/co/trabajos/ofertas-empleo-en-popayan',
+      title: 'Ofertas empleo en Popayan'
+    },
+    {
+      name: 'Bogota',
+      url: 'https://www-stg.magneto365.com/co/trabajos/ofertas-empleo-en-bogota',
+      title: 'Ofertas empleo en Bogota'
+    },
+    {
+      name: 'Monteria',
+      url: 'https://www-stg.magneto365.com/co/trabajos/ofertas-empleo-en-monteria',
+      title: 'Ofertas empleo en Monteria'
+    },
+    {
+      name: 'Popayan',
+      url: 'https://www-stg.magneto365.com/co/trabajos/ofertas-empleo-en-popayan',
+      title: 'Ofertas empleo en Popayan'
+    },
+    {
+      name: 'Bogota',
+      url: 'https://www-stg.magneto365.com/co/trabajos/ofertas-empleo-en-bogota',
+      title: 'Ofertas empleo en Bogota'
+    },
+    {
+      name: 'Monteria',
+      url: 'https://www-stg.magneto365.com/co/trabajos/ofertas-empleo-en-monteria',
+      title: 'Ofertas empleo en Monteria'
+    },
+    {
+      name: 'Popayan',
+      url: 'https://www-stg.magneto365.com/co/trabajos/ofertas-empleo-en-popayan',
+      title: 'Ofertas empleo en Popayan'
+    },
+    {
+      name: 'Bogota',
+      url: 'https://www-stg.magneto365.com/co/trabajos/ofertas-empleo-en-bogota',
+      title: 'Ofertas empleo en Bogota'
+    },
+    {
+      name: 'Monteria',
+      url: 'https://www-stg.magneto365.com/co/trabajos/ofertas-empleo-en-monteria',
+      title: 'Ofertas empleo en Monteria'
+    },
+    {
+      name: 'Popayan',
+      url: 'https://www-stg.magneto365.com/co/trabajos/ofertas-empleo-en-popayan',
+      title: 'Ofertas empleo en Popayan'
+    },
+    {
+      name: 'Bogota',
+      url: 'https://www-stg.magneto365.com/co/trabajos/ofertas-empleo-en-bogota',
+      title: 'Ofertas empleo en Bogota'
+    },
+    {
+      name: 'Monteria',
+      url: 'https://www-stg.magneto365.com/co/trabajos/ofertas-empleo-en-monteria',
+      title: 'Ofertas empleo en Monteria'
+    },
+    {
+      name: 'Popayan',
+      url: 'https://www-stg.magneto365.com/co/trabajos/ofertas-empleo-en-popayan',
+      title: 'Ofertas empleo en Popayan'
+    }
+  ],
+  isRemote: true,
+  textRemote: 'Remoto / híbrido en: '
+}
+
 export const CitiesDetailDrawerPropsWithoutUrl: ICitiesDetailDrawer = {
   cities: [
     {
