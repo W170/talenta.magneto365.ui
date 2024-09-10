082d21c777b8ab9dd2b8ee6ae4560c14087d5743
Santiago Granada <sgranada@talenta.cloud>
2024-09-09 19:55:04 -0500

    feat: update divider component

--- src/components/UI/atoms/Divider/Divider.component.tsx
+++ src/components/UI/atoms/Divider/Divider.component.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { IDivider } from './Divider.interface'
 import style from './divider.module.scss'
 
-const Component: React.FC = () => <div className={style.divider} />
+const Component: React.FC<IDivider> = ({ direction = 'HORIZONTAL' }) => {
+  const inlineStyle = direction == 'HORIZONTAL' ? { width: 'calc(100% - 10px)', height: 1 } : { width: 1 }
+  return <div style={inlineStyle} className={style.divider} />
+}
 
 /**
  * Atom UI Divider Component
