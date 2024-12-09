318a581ed91c74050bd7ee63f8c46ca821e13496
CastyL0 <clagarejo@magnetoglobal.com>
2024-12-09 12:00:30 -0500

    feat: update textAdapter.util.ts

--- src/utils/text/textAdapter.util.ts
+++ src/utils/text/textAdapter.util.ts
@@ -4,7 +4,10 @@ import { ISimilarCardData } from '@components/UI/molecules/SimilarCard'
 export const similarJobTextAdapter = (job: ISimilarCardData) => {
   const text: ICustomText[] = [
     { value: job.title, fontWeight: 'bold', lineBreak: true },
-    { value: `${job.companyName} | ${job.contractType}`, fontWeight: 'normal', lineBreak: true },
+    { value: job.contractType  ? `${job.companyName} | ${job.contractType}` : String(job.companyName), 
+      fontWeight: 'normal',
+      lineBreak: true,
+    },
     { value: job.salary || '', fontWeight: 'normal', lineBreak: true }
   ]
   return text
