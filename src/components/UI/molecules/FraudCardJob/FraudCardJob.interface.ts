4555e2a85bdc47439276d581b02ea433ea0242c1
Yeison Rodriguez <yrodriguez@talenta.cloud>
2024-12-17 15:44:47 -0500

    refactor: add fraudCardJob prop how optional

--- src/components/UI/molecules/FraudCardJob/FraudCardJob.interface.ts
+++ src/components/UI/molecules/FraudCardJob/FraudCardJob.interface.ts
@@ -2,18 +2,18 @@ export interface IFraudCardJob {
   /**
    * This is the text that display in the title of the card
    */
-  title: string
+  title?: string
   /**
    *This is the text that display in the body of the card
    */
-  content: string
+  content?: string
   /**
    * This is the text that display in the report button of the card
    */
-  linkText: string
+  linkText?: string
 
   /**
    * This is the link to redirect to the fraud form
    */
-  link: string
+  link?: string
 }
