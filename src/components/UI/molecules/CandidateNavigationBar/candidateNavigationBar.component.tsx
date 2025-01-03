e74f6b247ae51d52f5a95c0af6c370c9fe44a9d9
juandcobos <jdcobos@magnetoglobal.com>
2025-01-03 11:43:53 -0500

    feat: swipeDowm component

--- src/components/UI/molecules/CandidateNavigationBar/candidateNavigationBar.component.tsx
+++ src/components/UI/molecules/CandidateNavigationBar/candidateNavigationBar.component.tsx
@@ -4,6 +4,7 @@ import styles from './candidateNavigationBar.module.scss'
 import { classNames } from '@shared/utils/common'
 import { IconItem } from '@components/UI/atoms'
 import { ArrowLeft2, ArrowRight2 } from '@constants/icons.constants'
+import { SwipeDowm } from './children/swipeDowm/swipeDowm.component'
 
 const Component: React.FC<ICandidateNavigationBar> = ({
   className,
@@ -24,10 +25,15 @@ const Component: React.FC<ICandidateNavigationBar> = ({
 
   const activeMenu = active && 'candidateNavigationBarActive'
 
+  const handleSwipeDown = () => {
+    setActive(false)
+  }
+
   return (
     <div className={cx('candidateNavigationBar', `${activeMenu}`, className && className)}>
       {menuOptions && (
         <div className={cx('candidateNavigationBar__menu', `${active && 'candidateNavigationBar__menuActive'}`)}>
+          <SwipeDowm onSwipeDown={handleSwipeDown} />
           {menuOptions.map(({ icon, label, onClick }: ImenuOptions, index: number) => (
             <div
               onClick={() => onClick()}
@@ -68,6 +74,6 @@ const Component: React.FC<ICandidateNavigationBar> = ({
   )
 }
 /**
- * UI Atom component of Candidate Navigation Bar
+ *  Molecule UI component of Candidate Navigation Bar
  */
 export const CandidateNavigationBar = Component
