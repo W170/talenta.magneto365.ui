c4cd8449ec32f9b5f7caa90d9ff9a6e06d192b24
juandcobos <jdcobos@magnetoglobal.com>
2025-02-24 16:54:20 -0500

    feat: qualification component

---
+++ src/components/UI/atoms/Qualification/Qualification.interface.ts
@@ -0,0 +1,29 @@
+export interface IQualification  {
+    /**
+    * Component rating
+    */
+    rating?: number,
+    /**
+    * Function to change the rating value
+    */
+    onClick: (value: number) => void
+ }
+
+export interface IQualificationStar  {
+   /**
+   * alt icons
+   */
+  alt?: string
+  /**
+   * icons
+   */
+  icon?: string
+  /**
+   * icons hover
+   */
+  iconHover?: string
+  /**
+   * icons select
+   */
+  iconSelect?: string
+}
