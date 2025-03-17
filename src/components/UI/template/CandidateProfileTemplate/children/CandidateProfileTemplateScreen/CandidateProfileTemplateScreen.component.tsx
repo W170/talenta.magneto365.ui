7bd742b6c06f7da9cece4a24b530e19b3e877310
Luis Marín <lamarin@magnetoglobal.com>
2025-03-17 09:10:46 -0500

    fix: add padding bottom according to nav height

--- src/components/UI/template/CandidateProfileTemplate/children/CandidateProfileTemplateScreen/CandidateProfileTemplateScreen.component.tsx
+++ src/components/UI/template/CandidateProfileTemplate/children/CandidateProfileTemplateScreen/CandidateProfileTemplateScreen.component.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import { useCandidateProfile } from '../../CandidateProfileTemplate.context'
+import styles from './CandidateProfileTemplateScreen.module.scss'
+import { classNames } from '@shared/utils/common'
+
+const cx = classNames.bind(styles)
 
 const Component: React.FC<any> = ({ ...props }) => {
-  const { activeScreen, screens } = useCandidateProfile()
+  const { activeScreen, screens, navHeight } = useCandidateProfile()
   const { component: Screen } = screens[activeScreen] || {}
 
-  return <Screen {...props} />
+  return (
+    <div
+      className={cx('magneto-ui-candidate-profile-template-screen')}
+      style={
+        {
+          '--nav-height': `${navHeight}px`
+        } as React.CSSProperties
+      }
+    >
+      <Screen {...props} />
+    </div>
+  )
 }
 
 export const CandidateProfileTemplateScreen = Component
