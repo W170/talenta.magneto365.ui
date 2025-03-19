2512b4d065d333ed73608cd21f8978966ae81587
Luis Marín <lamarin@magnetoglobal.com>
2025-03-19 09:13:21 -0500

    refactor: improve typography documentation

--- src/components/UI/atoms/Typography/children/TypographyTitle/TypographyTitle.interface.ts
+++ src/components/UI/atoms/Typography/children/TypographyTitle/TypographyTitle.interface.ts
@@ -1,5 +1,11 @@
 import { ITypographyBase } from '../../Typography.interface'
 
 export interface ITypographyTitle extends ITypographyBase, Omit<React.HTMLAttributes<HTMLHeadingElement>, 'color'> {
+  /**
+   * Level of the html tag title.
+   *
+   * @default 2
+   * @example "1" | "2" | "3" | "4" | "5" | "6"
+   */
   level?: number
 }
