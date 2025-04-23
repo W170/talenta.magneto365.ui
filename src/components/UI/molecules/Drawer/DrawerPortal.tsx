75dc68a58249bdd5339fb7f655b5b58ebc00043a
alvarocartalenta <acardozo@talenta.cloud>
2025-04-23 17:43:57 -0500

    feat: implements container in every create portal in the app

--- src/components/UI/molecules/Drawer/DrawerPortal.tsx
+++ src/components/UI/molecules/Drawer/DrawerPortal.tsx
@@ -1,23 +1,25 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import ReactDOM from 'react-dom'
+import { ContainerContext } from '@components/context/container/container.context'
 import { IDrawerPortal } from './Drawer.interface'
 
 export const DrawerPortal: React.FC<IDrawerPortal> = ({ children }) => {
   const portalNode = useRef<Element | null>(null)
+  const { isServer, container } = useContext(ContainerContext)
   const [mounted, setMounted] = useState(false)
 
   useEffect(() => {
-    if (!document) return
+    if (isServer || !container) return
     portalNode.current = document.createElement('div')
     portalNode.current.classList.add('magneto-ui-drawer')
-    document.body.appendChild(portalNode.current)
+    container.appendChild(portalNode.current)
     setMounted(true)
     return () => {
       if (portalNode.current) {
-        document.body.removeChild(portalNode.current)
+        container.removeChild(portalNode.current)
       }
     }
-  }, [])
+  }, [container, isServer])
 
   return mounted && portalNode.current ? ReactDOM.createPortal(children, portalNode.current) : null
 }
--- src/components/UI/molecules/Modal/ModalPortal.component.tsx
+++ src/components/UI/molecules/Modal/ModalPortal.component.tsx
@@ -1,22 +1,24 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import ReactDOM from 'react-dom'
+import { ContainerContext } from '@components/context/container/container.context'
 import { IModalPortal } from './Modal.interface'
 
 export const ModalPortal: React.FC<IModalPortal> = ({ children }) => {
+  const { isServer, container } = useContext(ContainerContext)
   const portalNode = useRef<Element | null>(null)
   const [mounted, setMounted] = useState(false)
 
   useEffect(() => {
-    if (!document) return
+    if (isServer || !container) return
     portalNode.current = document.createElement('div')
     portalNode.current.classList.add('modal-ui-drawer')
-    document.body.appendChild(portalNode.current)
+    container.appendChild(portalNode.current)
     setMounted(true)
     return () => {
       if (portalNode.current) {
-        document.body.removeChild(portalNode.current)
+        container.removeChild(portalNode.current)
       }
     }
-  }, [])
+  }, [container, isServer])
   return mounted && portalNode.current ? ReactDOM.createPortal(children, portalNode.current) : null
 }
