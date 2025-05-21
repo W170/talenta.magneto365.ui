29c5a6fec121903cebebad5a12c9e047dceabf9c
bramos-magneto <bramos@magnetoglobal.com>
2025-05-21 11:51:47 -0500

    feat: create notification drawer component

---
+++ src/components/UI/organism/NotificationDrawerAnalyst/NotificationDrawerAnalyst.interface.ts
@@ -0,0 +1,11 @@
+export interface INotificationDrawerAnalystProps {
+  /**
+   * Boolean to show or hide the drawer
+   */
+  isDrawerOpen?: boolean
+
+  /**
+   * Function to close menu
+   */
+  onClose?: () => void
+}
