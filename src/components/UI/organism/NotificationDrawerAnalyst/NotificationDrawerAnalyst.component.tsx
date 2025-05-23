827d7a83bd19184accec01cac23ae7ae79b1d9ca
bramos-magneto <bramos@magnetoglobal.com>
2025-05-23 11:08:25 -0500

    feat: add header and mocked tabs to notification drawer

--- src/components/UI/organism/NotificationDrawerAnalyst/NotificationDrawerAnalyst.component.tsx
+++ src/components/UI/organism/NotificationDrawerAnalyst/NotificationDrawerAnalyst.component.tsx
@@ -1,6 +1,10 @@
 import React, { useCallback } from 'react'
+import { IconItem, Typography } from '@components/UI/atoms'
 import { Drawer } from '@components/UI/molecules'
+import { NotificationBlack } from '@constants/icons.constants'
 import { INotificationDrawerAnalystProps } from './NotificationDrawerAnalyst.interface'
+import styles from './NotificationDrawerAnalyst.module.scss'
+import { NotificationDrawerTabs } from './children'
 
 const Component: React.FC<INotificationDrawerAnalystProps> = ({ isDrawerOpen = false, onClose }) => {
   const handlerClose = useCallback(() => {
@@ -8,8 +12,16 @@ const Component: React.FC<INotificationDrawerAnalystProps> = ({ isDrawerOpen = f
   }, [onClose])
 
   return (
-    <Drawer isOpen={isDrawerOpen} onClose={handlerClose} direction="right">
-      NotificationDrawerAnalyst
+    <Drawer isOpen={isDrawerOpen} onClose={handlerClose} direction="right" drawerWidth="608px" customPadding={8}>
+      <header className={`${styles['magneto-ui-notification-drawer-analyst__header']}`}>
+        <IconItem alt={'notification icon'} icon={NotificationBlack} color="#292F37" />
+        <Typography.Title weight={'bold'} color="black" size={'lg'}>
+          {'Notificaciones'}
+        </Typography.Title>
+      </header>
+      <NotificationDrawerTabs
+        sections={['Todo', 'Solicitudes', 'Gestión de candidatos', 'Estados de vacante', 'Entrevistas']}
+      />
     </Drawer>
   )
 }
