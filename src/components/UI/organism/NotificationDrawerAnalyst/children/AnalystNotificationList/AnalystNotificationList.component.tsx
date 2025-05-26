8213642f93ef84a1ed4e3c97a5d8dde141b04055
Magudelo-magneto <magudelo@magnetoglobal.com>
2025-05-26 10:20:37 -0500

    feat: implement mock infinite scroll while waiting for API endpoint

--- src/components/UI/organism/NotificationDrawerAnalyst/children/AnalystNotificationList/AnalystNotificationList.component.tsx
+++ src/components/UI/organism/NotificationDrawerAnalyst/children/AnalystNotificationList/AnalystNotificationList.component.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
-import { IAnalystNotificationListProps } from './AnalystNotificationList.interface'
 import { AnalystNotificationCard } from './children/AnalystNotificationCard.component'
 import styles from './AnalystNotificationList.module.scss'
+import { useInfiniteNotifications } from '../../hooks'
+import { FlatLoader } from '@components/UI/atoms'
 
-export function AnalystNotificationList({ notifications }: IAnalystNotificationListProps) {
+export function AnalystNotificationList() {
+  const { notifications, lastNotificationRef, isLoading } = useInfiniteNotifications()
+
   return (
     <div className={styles.analystNotificationList}>
-      {notifications.map((notification) => (
-        <AnalystNotificationCard key={notification.id} notification={notification} />
-      ))}
+      {notifications.map((notification, index) => {
+        const isLast = index === notifications.length - 1
+        return (
+          <div key={notification.id} ref={isLast ? lastNotificationRef : null}>
+            <AnalystNotificationCard notification={notification} />
+          </div>
+        )
+      })}
+      {isLoading && (
+        <div className={styles.loader}>
+          <FlatLoader color="#001B38" secondColor="transparent" size={45} />
+        </div>
+      )}
     </div>
   )
 }
---
+++ src/components/UI/organism/NotificationDrawerAnalyst/hooks/index.ts
@@ -0,0 +1,1 @@
+export * from './useInfiniteNotifications.hook'
