7380ff69c92c753c5ce3cca04ee29764b2e9072a
alvarocartalenta <acardozo@talenta.cloud>
2025-07-09 07:16:33 -0500

    refactor: remove styles file call from job details drawer

--- src/components/UI/organism/JobDetails/children/JobDetailsDrawer/JobDetailsDrawer.component.tsx
+++ src/components/UI/organism/JobDetails/children/JobDetailsDrawer/JobDetailsDrawer.component.tsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import { Drawer } from '@components/UI/molecules'
-import { classNames } from '@shared/utils/common'
 import { IJobDetailsDrawer } from './JobDetailsDrawer.interface'
-import styles from './JobDetailsDrawer.module.scss'
-
-const cx = classNames.bind(styles)
 
 export const JobDetailsDrawer: React.FC<IJobDetailsDrawer> = (props) => (
   <Drawer
     {...props}
     customPadding={0}
-    className={cx('drawer', props.className)}
+    className={props.className}
     hideButton
     direction="right"
     drawerWidth="fit-content"
