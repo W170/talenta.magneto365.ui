0efc5ee6456cb2a3b8cbeee1d459f32b0ec89a43
Carlos <cecheverri@magnetoglobal.com>
2025-09-08 08:50:12 -0500

    fix: updated styles to give stiles from   registry proyect

--- src/components/Domain/Jobs/JobDetails/children/JobSuggestedCard/JobSuggestedCard.component.tsx
+++ src/components/Domain/Jobs/JobDetails/children/JobSuggestedCard/JobSuggestedCard.component.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import styles from './JobSuggestedCard.module.scss'
-import { classNames } from '@shared/utils/common'
 import { IJobSuggestedCard } from './JobSuggestedCard.interface'
 import { JobSuggestedSection } from './children/JobSuggestedSection'
 
-const cx = classNames.bind(styles)
 const Component: React.FC<IJobSuggestedCard> = ({ children, className }) => {
-  return <div className={cx('job-suggested-card', className)}>{children}</div>
+  return <div className={`${styles['job-suggested-card']} ${className || ''}`}>{children}</div>
 }
 
 export const JobSuggestedCard = Object.assign(Component, {
