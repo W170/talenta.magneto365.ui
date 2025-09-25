dd687b510312282a5d95277c14c416ce9c0ec0cd
Yeison Rodriguez <yrodriguez@talenta.cloud>
2025-09-25 18:06:47 -0500

    refactor: update component and styles

--- src/components/UI/molecules/CreateCustomAlert/CreateCustomAlert.component.tsx
+++ src/components/UI/molecules/CreateCustomAlert/CreateCustomAlert.component.tsx
@@ -1,26 +1,25 @@
 import React from 'react'
 import { ICreateCustomAlert } from './CreateCustomAlert.interface'
 import { ArrowRight2, Message3D } from '@constants/icons.constants'
-import { DotsLoader } from '@components/UI/atoms'
+import { Button, DotsLoader } from '@components/UI/atoms'
 import styles from './createCustomAlert.module.scss'
 
 const Component: React.FC<ICreateCustomAlert> = ({ onClick, title, isLoading }) => {
   return (
-    <button disabled={isLoading} onClick={onClick} className={styles['create-custom-alert']}>
+    <div onClick={onClick} className={styles['create-custom-alert']}>
       {isLoading ? (
         <div className={styles['create-custom-alert__loader']}>
           <DotsLoader color="#1a324c" count={3} />
         </div>
       ) : (
-        <>
-          <img className={styles['create-custom-alert__icon']} src={Message3D} alt="avatar" />
-          <div className={styles['create-custom-alert__summary']}>
-            <h4>{title}</h4>
-          </div>
-          <img className={styles['create-custom-alert__arrow']} src={ArrowRight2} alt="arrow" />
-        </>
+        <Button
+          className={styles['create-custom-alert__button']}
+          prefixIcon={ArrowRight2}
+          buttonText={title}
+          suffixIcon={Message3D}
+        />
       )}
-    </button>
+    </div>
   )
 }
 
