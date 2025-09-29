8675fc90b4c48caac52c733fc5c8ce100d73106e
Carlos <cecheverri@magnetoglobal.com>
2025-09-29 11:26:23 -0500

    fix: order imports

--- src/components/UI/organism/ProcessesCard/children/ProcessesCardArrow/ProcessesCardArrow.component.tsx
+++ src/components/UI/organism/ProcessesCard/children/ProcessesCardArrow/ProcessesCardArrow.component.tsx
@@ -3,6 +3,7 @@ import { classNames } from '@shared/utils/common'
 import styles from '../../ProcessesCard.module.scss'
 import { IProcessesCard } from '../../ProcessesCard.interface'
 import { ArrowRightBlack } from '@constants/icons.constants'
+
 const cx = classNames.bind(styles)
 
 const Component: React.FC<IProcessesCard.IArrow> = ({ className, status, ...props }) => {
