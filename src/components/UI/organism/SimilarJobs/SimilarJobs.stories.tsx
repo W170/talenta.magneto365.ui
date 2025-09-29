b46048898418f0d2fd013f2607ced4bf0b9777c7
Alvaro Cardozo <acardozo@magnetoglobal.com>
2025-09-29 14:23:43 -0500

    refactor: bring back similar job

---
+++ src/components/UI/organism/SimilarJobs/SimilarJobs.stories.tsx
@@ -0,0 +1,33 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { SimilarJobs } from '.'
+import {
+  moreJobsText,
+  sectionTitle,
+  similarVacanciesCardsProps,
+  moreJobsLink,
+  hideLogo
+} from '@constants/stories/similarJobs.constants'
+
+const meta: Meta<typeof SimilarJobs> = {
+  title: 'Organism/Similar Jobs',
+  component: SimilarJobs,
+  tags: ['autodocs'],
+  args: {
+    moreJobsLink,
+    moreJobsText,
+    sectionTitle,
+    hideLogo,
+    similarJobsCard: similarVacanciesCardsProps
+  }
+}
+
+export default meta
+
+type Story = StoryObj<typeof SimilarJobs>
+
+export const Default: Story = {}
+export const HideLogo: Story = {
+  args: {
+    hideLogo: true
+  }
+}
