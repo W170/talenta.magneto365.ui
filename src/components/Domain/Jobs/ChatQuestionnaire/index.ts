e3f0df61cc5618fc4787f97748332510ca79ed56
Alvaro Cardozo <acardozo@magnetoglobal.com>
2025-10-06 12:28:10 -0500

    refactor: organize modules to library export

--- src/components/Domain/Jobs/ChatQuestionnaire/index.ts
+++ src/components/Domain/Jobs/ChatQuestionnaire/index.ts
@@ -1,19 +1,30 @@
-import { ChatQuestionnaire as RawChatQuestionnaire } from './ChatQuestionnaire.component';
-import { ChatQuestionnaireMessageCandidate } from './children/ChatQuestionnaireMessageCandidate';
-import { ChatQuestionnaireMultipleChoice } from './children/ChatQuestionnaireMultipleChoice';
-import { ChatQuestionnaireOpen } from './children/ChatQuestionnaireOpen/ChatQuestionnaireOpen.component';
-import { ChatQuestionnaireQuestion } from './children/ChatQuestionnaireQuestion';
-import { ChatQuestionnaireUniqueChoice } from './children/ChatQuestionnaireUniqueChoice';
-import { useChatQuestionnaire } from './hooks/useChatQuestionnaire';
-export type { IChatQuestionnaire, IQuestionWithAnswer, TSendQuestion, TSendCloseQuestion } from './ChatQuestionnaire.interface';
+import { ChatQuestionnaire as RawChatQuestionnaire } from './ChatQuestionnaire.component'
+import { ChatQuestionnaireMessageCandidate } from './children/ChatQuestionnaireMessageCandidate'
+import { ChatQuestionnaireMultipleChoice } from './children/ChatQuestionnaireMultipleChoice'
+import { ChatQuestionnaireOpen } from './children/ChatQuestionnaireOpen/ChatQuestionnaireOpen.component'
+import { ChatQuestionnaireQuestion } from './children/ChatQuestionnaireQuestion'
+import { ChatQuestionnaireUniqueChoice } from './children/ChatQuestionnaireUniqueChoice'
+import { useChatQuestionnaire } from './hooks/useChatQuestionnaire'
+export type {
+  IChatQuestionnaire,
+  IQuestionWithAnswer,
+  TSendQuestion,
+  TSendCloseQuestion,
+  EQuestionType,
+  IPossibleAnswer,
+  IQuestion,
+  IQuestionnaireAnswer,
+  IQuestionnaireMessage,
+  TSendOpenQuestion
+} from './ChatQuestionnaire.interface'
 
 export const ChatQuestionnaire = Object.assign(RawChatQuestionnaire, {
-    Question: ChatQuestionnaireQuestion,
-    MessageCandidate: ChatQuestionnaireMessageCandidate,
-    Answer: {
-        UniqueChoice: ChatQuestionnaireUniqueChoice,
-        MultipleChoice: ChatQuestionnaireMultipleChoice,
-        Open: ChatQuestionnaireOpen,
-    },
-    useChat: useChatQuestionnaire,
+  Question: ChatQuestionnaireQuestion,
+  MessageCandidate: ChatQuestionnaireMessageCandidate,
+  Answer: {
+    UniqueChoice: ChatQuestionnaireUniqueChoice,
+    MultipleChoice: ChatQuestionnaireMultipleChoice,
+    Open: ChatQuestionnaireOpen
+  },
+  useChat: useChatQuestionnaire
 })
