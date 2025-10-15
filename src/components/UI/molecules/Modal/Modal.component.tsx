55479dd57c0bb5783860e522e303369fbd226d39
Carlos <cecheverri@magnetoglobal.com>
2025-10-15 10:00:57 -0500

    fix: reset overflow when dismount component

--- src/components/UI/molecules/Modal/Modal.component.tsx
+++ src/components/UI/molecules/Modal/Modal.component.tsx
@@ -18,6 +18,14 @@ const Modal: React.FC<IModal> = ({
   useEffect(() => {
     const { body } = document
     if (!body) return
+    return () => {
+      body.style.overflowY = 'auto'
+    }
+  }, [])
+
+  useEffect(() => {
+    const { body } = document
+    if (!body) return
     body.style.overflowY = isOpen ? 'hidden' : 'auto'
   }, [isOpen])
 
