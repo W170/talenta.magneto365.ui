ada84369fc32034e2281b10b15577fff9056a250
Luis Marín <lamarin@magnetoglobal.com>
2025-11-07 08:52:26 -0500

    refactor: add initial state to Field.List context

--- src/components/UI/atoms/Field/children/FieldList/FieldList.context.tsx
+++ src/components/UI/atoms/Field/children/FieldList/FieldList.context.tsx
@@ -1,7 +1,13 @@
 import { createContext, useContext } from 'react'
 import { IFieldListContext } from './FieldList.interface'
 
-export const ListContext = createContext<IFieldListContext | null>(null)
+const initialValue: IFieldListContext = {
+  isInsideList: false,
+  toggleValue: () => undefined,
+  value: undefined
+}
+
+export const ListContext = createContext<IFieldListContext>(initialValue)
 
 export const useFieldListContext = () => {
   return useContext(ListContext)
