4d423960afd3848397089d402dcba4bac180955e
Yeison Rodriguez <yrodriguez@talenta.cloud>
2026-02-20 10:32:24 -0500

    refactor: update imports

--- src/components/UI/molecules/MegaMenuSearchBar/index.ts
+++ src/components/UI/molecules/MegaMenuSearchBar/index.ts
@@ -1,4 +1,2 @@
-export * from './MegaMenuSearchBar.component'
 export { IMegaMenuSearchBar, ISearchBar } from './MegaMenuSearchBar.interface'
-export * from './MegaMenuSearchBar.stories'
 export { default as SearchBar } from './SearchBar.component'
