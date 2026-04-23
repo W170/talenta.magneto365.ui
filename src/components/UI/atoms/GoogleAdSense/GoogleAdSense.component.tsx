f727c08831d58dae1932771dacbb5d4de3d0eb18
Alvaro Cardozo <acardozo@magnetoglobal.com>
2026-04-23 08:24:24 -0500

    fix: add adsbygoogle object and push new element

--- src/components/UI/atoms/GoogleAdSense/GoogleAdSense.component.tsx
+++ src/components/UI/atoms/GoogleAdSense/GoogleAdSense.component.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { IAd } from './AdSense.interface'
 import { sizeMap } from '@constants/googleAdSense.constants'
 
@@ -7,6 +7,12 @@ import { sizeMap } from '@constants/googleAdSense.constants'
  */
 const Component: React.FC<IAd> = ({ size, style, className, ...props }) => {
   const sizeStyles = size ? sizeMap[size] : {}
+
+  useEffect(() => {
+    const adsbygoogle = (window as unknown as Record<string, unknown>).adsbygoogle || []
+    ;(adsbygoogle as unknown[]).push({})
+  }, [])
+
   return <ins className={`adsbygoogle ${className ?? ''}`} style={{ ...sizeStyles, ...style }} {...props} />
 }
 
